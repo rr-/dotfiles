0421aff0fcfdb18fefee6a0ff4ab343e0db4dff4
Dash <dash@bad.pink>
2013-06-19 13:46:26 +0200

    Moved date utils to lib

---
+++ lib/dateutil.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+def date_range(start_date, end_date):
+	for n in range((end_date - start_date).days + 1):
+		yield start_date + datetime.timedelta(days=n)
+
+def get_month_day_range(date):
+	first_day = datetime.date(date.year, date.month, 1)
+	nm = datetime.date(date.year, date.month, 15) + datetime.timedelta(days=31)
+	last_day = datetime.date(nm.year, nm.month, 1) - datetime.timedelta(days=1)
+	return date_range(first_day, last_day)
