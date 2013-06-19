190fed5fc25dd7b926c478f890c2ea3d25b079f0
Dash <dash@bad.pink>
2013-06-19 14:35:17 +0200

    Better usability, removed redundant code

--- datediff.py
+++ datediff.py
@@ -2,12 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import dateutil.parser
 from calendar import monthrange
 from datetime import datetime, timedelta
-
-if len(sys.argv) == 1:
-	print >>sys.stderr, 'No date specified'
-	sys.exit(1)
 
 def monthdelta(d1, d2):
 	delta = 0
@@ -20,32 +17,29 @@ def monthdelta(d1, d2):
 			break
 	return delta
 
-def parse(s):
-	if '-' not in s:
-		s = datetime.strftime(datetime.now(), '%Y-%m-%d ') + s
-	try:
-		return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
-	except:
-		try:
-			return datetime.strptime(s, '%Y-%m-%d %H:%M')
-		except:
-			return datetime.strptime(s, '%Y-%m-%d')
+if len(sys.argv) == 1:
+	print >>sys.stderr, 'No date specified'
+	sys.exit(1)
 
-date1 = parse(sys.argv[1])
-if len(sys.argv) == 2:
-	date2 = datetime.now()
-else:
-	date2 = parse(sys.argv[2])
-print date1
+try:
+	date1 = dateutil.parser.parse(sys.argv[1])
+	if len(sys.argv) == 2:
+		date2 = datetime.now()
+	else:
+		date2 = dateutil.parser.parse(sys.argv[2])
+except ValueError:
+	print >>sys.stderr, 'Invalid date format'
+	sys.exit(1)
 
-diff = date1 - date2
+diff = date2 - date1
+print date2, '-', date1
 print 'Seconds: ' + str(diff.days * 24 * 60 * 60 + diff.seconds)
 print 'Minutes: ' + str(diff.days * 24 * 60 + diff.seconds / 60)
 print 'Hours:   ' + str(diff.days * 24 + diff.seconds / 60 / 60)
 print 'Days:    ' + str(diff.days)
-if date2 < date1:
-	print 'Months:  ' + str(monthdelta(date2, date1))
-	print 'Years:   ' + str(monthdelta(date2, date1) / 12)
+if date1 < date2:
+	print 'Months:  ' + str(monthdelta(date1, date2))
+	print 'Years:   ' + str(monthdelta(date1, date2) / 12)
 else:
-	print 'Months:  -' + str(monthdelta(date1, date2))
-	print 'Years:   -' + str(monthdelta(date1, date2) / 12)
+	print 'Months:  -' + str(monthdelta(date2, date1))
+	print 'Years:   -' + str(monthdelta(date2, date1) / 12)
