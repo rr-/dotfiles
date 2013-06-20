42dd15078e26b7878784ee7c518e3b410018ce57
Marcin Kurczewski <mkurczew@gmail.com>
2013-06-20 11:06:18 +0200

    Fixed constants

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ cmd='rsync -azK --delete-after -e ssh'
 $cmd $user@$server:/etc/ ~/clutter/backup-$server/etc
 $cmd $user@$server:/home/rr-/ ~/clutter/backup-$server/home/rr-/ --exclude img/ --exclude backup-$desktop/
 $cmd $user@$server:/home/srv/ ~/clutter/backup-$server/home/srv/ --exclude www/mal-dev/
-$cmd ~/clutter/ $user@$server:~/backup-luna/clutter/ --exclude backup-$server/
-$cmd ~/img/ $user@$server:~/backup-luna/img/ --exclude net/
-$cmd ~/mgr/ $user@$server:~/backup-luna/mgr/
-$cmd ~/text/ $user@$server:~/backup-luna/text/
-$cmd ~/src/ $user@$server:~/backup-luna/src/
+$cmd ~/clutter/ $user@$server:~/backup-$desktop/clutter/ --exclude backup-$server/
+$cmd ~/img/ $user@$server:~/backup-$desktop/img/ --exclude net/
+$cmd ~/mgr/ $user@$server:~/backup-$desktop/mgr/
+$cmd ~/text/ $user@$server:~/backup-$desktop/text/
+$cmd ~/src/ $user@$server:~/backup-$desktop/src/
