d6849a9bc4a2e66b09c0f1401ff3bae7167dc3fd
Marcin Kurczewski <mkurczew@gmail.com>
2013-06-20 09:54:56 +0200

    Moved code around

--- imgop.py
+++ imgop.py
@@ -4,6 +4,24 @@ import os
 import sys
 import subprocess
 
+def backup(file):
+	backup = file + '~'
+	if os.path.exists(backup):
+		raise Exception(backup + ' already exists')
+	os.rename(file, backup)
+	return backup, file
+
+def transferFileStats(src, dst):
+	atime = os.path.getatime(src) #this is changed immediately after rename
+	mtime = os.path.getmtime(src)
+	os.utime(dst, (atime, mtime))
+
+def call(cmd):
+	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+	out = proc.communicate()[0]
+	return out
+
+
 class Operation(object):
 	def run_all(self, files):
 		for file in files:
@@ -17,59 +35,39 @@ class Operation(object):
 			except Exception as e:
 				print >>sys.stderr, str(e)
 
-	def backup(self, file):
-		backup = file + '~'
-		if os.path.exists(backup):
-			raise Exception(backup + ' already exists')
-		os.rename(file, backup)
-		return backup, file
-
-	def transferFileStats(self, src, dst):
-		atime = os.path.getatime(src) #this is changed immediately after rename
-		mtime = os.path.getmtime(src)
-		os.utime(dst, (atime, mtime))
-
-	def call(self, cmd):
-		proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-		out = proc.communicate()[0]
-		return out
-
-
 
 class DegradeOperation(Operation):
 	name = ['degrade', 'downgrade']
 
 	def run(self, file):
-		backup, file = self.backup(file)
+		backup, file = makeBackup(file)
 		new = os.path.splitext(file)[0] + '.jpg'
-		cmd = [
-			'convert',
-			'%s[0]' % backup,
-			'-quality', '80',
-			'jpg:' + new]
-		self.call(cmd)
-		self.transferFileStats(backup, file)
+		cmd  = ['convert']
+		cmd += ['%s[0]' % backup]
+		cmd += ['-quality', '80']
+		cmd += ['jpg:' + new]
+		call(cmd)
+		transferFileStats(backup, file)
 
 
 class FixAnamorphicOperation(Operation):
 	name = ['fix-anamorphic']
 
 	def run(self, file):
-		cmd = [
-			'identify',
-			'-format', '%w %h',
-			file + '[0]']
-		out = self.call(cmd)
+		cmd  = ['identify']
+		cmd += ['-format', '%w %h']
+		cmd += [file + '[0]']
+		out = call(cmd)
 		w, h = map(int, out.split(' '))
 		nw = h * 16 // 9
 		nh = h
-		backup, file = self.backup(file)
-		cmd = ['convert',
-			'%s[0]' % backup,
-			'-resize', '%dx%d!' % (nw, nh),
-			file]
-		self.call(cmd)
-		self.transferFileStats(backup, file)
+		backup, file = makeBackup(file)
+		cmd  = ['convert']
+		cmd += ['%s[0]' % backup]
+		cmd += ['-resize', '%dx%d!' % (nw, nh)]
+		cmd += [file]
+		call(cmd)
+		transferFileStats(backup, file)
 
 
 class FixPngOperation(Operation):
@@ -79,16 +77,16 @@ class FixPngOperation(Operation):
 		_, extension = os.path.splitext(file)
 		if extension.lower() != '.png':
 			raise Exception(file + ' is not a PNG file')
-		cmd = ['identify',
-			'-format', '%r',
-			file]
-		out = self.call(cmd)
+		cmd  = ['identify']
+		cmd += ['-format', '%r']
+		cmd += [file]
+		out = call(cmd)
 		if out.strip() == 'PseudoClassGrayMatte':
-			cmd = ['convert',
-				file,
-				'-alpha', 'off',
-				file]
-			self.call(cmd)
+			cmd  = ['convert']
+			cmd += [file]
+			cmd += ['-alpha', 'off']
+			cmd += [file]
+			call(cmd)
 
 
 
