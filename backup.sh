be61e641919e3350c749410ba9e6811560d4e37e
Marcin Kurczewski <mkurczew@gmail.com>
2013-06-27 14:25:46 +0200

    Fixed paths

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 [ "$(hostname)" != "$desktop" ] && echo "Must be run on $desktop" 1>&2 && exit 1
 
 cmd='rsync -azK --delete-after -e ssh'
-$cmd "$user@$server:/etc/" "~/clutter/backup-$server/etc"
-$cmd "$user@$server:/home/$user/ ~/clutter/backup-$server/home/$user/" --exclude "img/" --exclude "backup-$desktop/"
-$cmd "$user@$server:/home/srv/ ~/clutter/backup-$server/home/srv/" --exclude "www/mal-dev/"
-$cmd "~/clutter/" "$user@$server:~/backup-$desktop/clutter/" --exclude "backup-$server/"
-$cmd "~/img/" "$user@$server:~/backup-$desktop/img/" --exclude "net/"
-$cmd "~/mgr/" "$user@$server:~/backup-$desktop/mgr/"
-$cmd "~/text/" "$user@$server:~/backup-$desktop/text/"
-$cmd "~/src/" "$user@$server:~/backup-$desktop/src/"
+$cmd "$user@$server:/etc/" "/cygdrive/z/clutter/backup-$server/etc"
+$cmd "$user@$server:/home/$user/" "/cygdrive/z/clutter/backup-$server/home/$user/" --exclude "img/" --exclude "backup-$desktop/"
+$cmd "$user@$server:/home/srv/" "/cygdrive/z/clutter/backup-$server/home/srv/" --exclude "www/mal-dev/"
+$cmd "/cygdrive/z/clutter/" "$user@$server:/home/$user/backup-$desktop/clutter/" --exclude "backup-$server/"
+$cmd "/cygdrive/z/img/" "$user@$server:/home/$user/backup-$desktop/img/" --exclude "net/"
+$cmd "/cygdrive/z/mgr/" "$user@$server:/home/$user/backup-$desktop/mgr/"
+$cmd "/cygdrive/z/text/" "$user@$server:/home/$user/backup-$desktop/text/"
+$cmd "/cygdrive/z/src/" "$user@$server:/home/$user/backup-$desktop/src/"
