dafb0e620b1ed20bd6186fcec3b3a342c15b5d89
Dash <dash@bad.pink>
2013-07-01 11:52:11 +0200

    Added cleanup on cygwin

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s "$DIR/bashcfg.sh" "$HOME/.bashrc"
 ln -s "$DIR/vimcfg.vim" "$HOME/.vimrc"
 ln -s "$DIR/vim" "$HOME/.vim"
+
+shopt -s nocasematch
+if [[ "$(uname)" =~ cygwin ]]; then
+	for x in .bashrc .vimrc .vim; do
+		winpath=$(cygpath -w "$HOME")\\$x
+		attrib +h +s "$winpath"
+	done
+fi
