ab7ebcbfb15b4d5793273352f193ebba6470da3d
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-01 12:55:12 +0200

    Lib/ no longed needed

--- lib/proc.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def execute(cmd):
-	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-	out, err = proc.communicate()
-	return (proc.returncode, out, err)
--- working_hours.py
+++ working_hours.py
@@ -1,11 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import datetime
 import getpass
 import locale
 import gdata.calendar.client
 from pyExcelerator import *
-from lib.dt import get_month_day_range, parse
+from dateutil.parser import parse
+
+def date_range(start_date, end_date):
+	for n in range((end_date - start_date).days + 1):
+		yield start_date + datetime.timedelta(days=n)
+
+def get_month_day_range(date):
+	first_day = datetime.date(date.year, date.month, 1)
+	nm = datetime.date(date.year, date.month, 15) + datetime.timedelta(days=31)
+	last_day = datetime.date(nm.year, nm.month, 1) - datetime.timedelta(days=1)
+	return date_range(first_day, last_day)
 
 class GodzinyMaker(object):
 	def __init__(self, mail, password, keyword, path):
