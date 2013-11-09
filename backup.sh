088b335ca3c12f3697a8c5d086f9ffb96e77a1d0
Marcin Kurczewski <mkurczew@gmail.com>
2013-11-09 16:09:30 +0100

    Added software to backup script

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 
 backup_user=backup
 server_backup=( /etc "/home/$user" /home/srv )
-desktop_backup=( clutter img text src )
+desktop_backup=( software/utilities clutter img text src )
 
 for x in ${server_backup[@]}; do
 	echo "$server --> $desktop: $x"
@@ -14,6 +14,6 @@ done
 
 for x in ${desktop_backup[@]}; do
 	echo "$desktop --> $server: $x"
-	rsync --chmod=D=rwxrxrx,F=rwrr -avzKR --delete-during -e "ssh -i /home/rr-/.ssh/id_rsa" "/cygdrive/z/$x/" "$backup_user@$server_addr:/home/backup/backup-$desktop/" --exclude "/cygdrive/z/img/net/"
+	rsync --chmod=D=rwxrxrx,F=rwrr -avzKR --delete-during -e "ssh -i /home/rr-/.ssh/id_rsa" "/cygdrive/z/$x/" "$backup_user@$server_addr:/home/backup/backup-$desktop/" --exclude "/cygdrive/z/img/net/" --exclude "/cygdrive/z/software/utilities/cygwin"
 	echo
 done
