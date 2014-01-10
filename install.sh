e1a54b92e6a4f9af5df2f13db1c43ed391a7492b
Dash <dash@bad.pink>
2014-01-10 11:27:30 +0100

    Added .inputrc

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-for x in .bashrc .vimrc .vim; do
+for x in .bashrc .vimrc .vim .inputrc; do
 	mv "$HOME/$x" "$HOME/$x~"
 done
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s "$DIR/bashcfg.sh" "$HOME/.bashrc"
 ln -s "$DIR/vimcfg.vim" "$HOME/.vimrc"
 ln -s "$DIR/vim" "$HOME/.vim"
+ln -s "$DIR/inputrc" "$HOME/.inputrc"
 
 shopt -s nocasematch
 if [[ "$(uname)" =~ cygwin ]]; then
-	for x in .bashrc .vimrc .vim; do
+	for x in .bashrc .vimrc .vim .inputrc; do
 		winpath=$(cygpath -w "$HOME")\\$x
 		attrib +h +s "$winpath"
 	done
