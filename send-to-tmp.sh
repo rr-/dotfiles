660898a38009ec1cee7c487d8d3f3a65c3038e6b
Dash <dash@bad.pink>
2014-01-26 18:03:11 +0100

    send-to-tmp can be run from any machine now

---
+++ send-to-tmp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dst_folder=/srv/www/tmp.sakuya.pl/public_html/f/
+base_url=http://tmp.sakuya.pl/f/
+remote_user=rr-
+remote_addr=burza
+
+ssh -n "$remote_user@$remote_addr" "mkdir -p \"$dst_folder\"; chmod 0755 \"$dst_folder\""
+
+for src_path in "$@"; do
+	if [ ! -f "$src_path" ]; then
+		echo "$src_path not found" 2>&1
+	else
+		dst_path="$dst_folder"${src_path##*/}
+
+		src_quoted=$(echo "$src_path"|sed 's/[ ()]/\0/g;')
+		dst_quoted=$(echo "$dst_path"|sed 's/[ ()]/\\\0/g;')
+
+		scp -q "$src_quoted" "$remote_user@$remote_addr:$dst_quoted"
+		ssh -n "$remote_user@$remote_addr" "touch \"$dst_path\"; chmod 0644 \"$dst_path\""
+
+		echo "${base_url}${src_path##*/}"
+	fi
+done
