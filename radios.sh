5e295dd1facc2905659e76003f27ea3b9b20375e
Dash <dash@bad.pink>
2014-02-21 11:56:53 +0100

    Added script for restoring radios to foobar

---
+++ radios.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function find_exe()
+{
+	exe="$1"
+	shift
+	for x in "$@"; do
+		if [ -f "$x/$exe" ]; then
+			echo "$x/$exe"
+		fi
+	done
+}
+
+foobar_path=$(find_exe foobar2000.exe \
+	'C:/Program Files/foobar2000' \
+	'C:/Program Files (x86)/foobar2000')
+
+if [ -z "$foobar_path" ]; then
+	echo Foobar not found! 1>&2
+	exit 1
+fi
+
+while read line; do
+	url=$(echo "$line"|sed 's/\s*;.*$//g')
+	"$foobar_path" /add "$url"
+done < "radios.lst"
