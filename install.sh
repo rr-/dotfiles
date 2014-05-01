e20878de595b8076042093cf33d606dd7610caa5
Marcin Kurczewski <mkurczew@gmail.com>
2014-05-01 19:28:48 +0200

    Added MinTTY configuration file

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ ln -s "$DIR/bashcfg.sh" "$HOME/.bashrc"
 ln -s "$DIR/vimcfg.vim" "$HOME/.vimrc"
 ln -s "$DIR/vim" "$HOME/.vim"
 ln -s "$DIR/inputrc" "$HOME/.inputrc"
+ln -s "$DIR/minttyrc" "$HOME/minttyrc"
 
 shopt -s nocasematch
 if [[ "$(uname)" =~ cygwin ]]; then
-	for x in .bashrc .vimrc .vim .inputrc; do
+	for x in .bashrc .vimrc .vim .inputrc .minttyrc; do
 		winpath=$(cygpath -w "$HOME")\\$x
 		attrib +h +s "$winpath"
 	done
