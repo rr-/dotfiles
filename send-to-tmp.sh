6e0b2af6e134b7355e4a855b3535082cd840a8ae
Marcin Kurczewski <mkurczew@gmail.com>
2014-06-04 09:46:45 +0200

    More portable sending to tmp

--- send-to-tmp.sh
+++ send-to-tmp.sh
@@ -3,9 +3,10 @@
 dst_folder=/srv/www/tmp.sakuya.pl/public_html/f/
 base_url=http://tmp.sakuya.pl/f/
 remote_user=rr-
-remote_addr=burza
+remote_addr=tmp.sakuya.pl
+remote_port=65000
 
-ssh -n "$remote_user@$remote_addr" "mkdir -p \"$dst_folder\"; chmod 0755 \"$dst_folder\""
+ssh -p"$remote_port" -n "$remote_user@$remote_addr" "mkdir -p \"$dst_folder\"; chmod 0755 \"$dst_folder\""
 
 for src_path in "$@"; do
 	if [ ! -f "$src_path" ]; then
@@ -16,8 +17,8 @@ for src_path in "$@"; do
 		src_quoted=$(echo "$src_path"|sed 's/[ ()]/\0/g;')
 		dst_quoted=$(echo "$dst_path"|sed 's/[ ()]/\\\0/g;')
 
-		scp -q "$src_quoted" "$remote_user@$remote_addr:$dst_quoted"
-		ssh -n "$remote_user@$remote_addr" "touch \"$dst_path\"; chmod 0644 \"$dst_path\""
+		scp -P"$remote_port" -q "$src_quoted" "$remote_user@$remote_addr:$dst_quoted"
+		ssh -p"$remote_port" -n "$remote_user@$remote_addr" "touch \"$dst_path\"; chmod 0644 \"$dst_path\""
 
 		echo "${base_url}${src_path##*/}"
 	fi
