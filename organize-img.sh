f278a8128bc00fa27db82dcf84750f88d515a9e0
Marcin Kurczewski <mkurczew@gmail.com>
2014-10-11 16:06:49 +0200

    Improved image organizer performance a lot

--- organize-img.sh
+++ organize-img.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-shopt -s nocasematch
 remote_root_dir="/home/rr-/img/"
 dst_root_dir="/cygdrive/z/img/net/"
 remote_user=rr-
@@ -12,6 +10,8 @@ min_size=$[$min_width*$min_height]
 
 [ "$(hostname)" != "$local_addr" ] && echo "Must be run on $local_addr" 1>&2 && exit 1
 
+shopt -s extglob
+shopt -s nocasematch
 trap "exit" INT
 
 #prepare transit dir
@@ -21,33 +21,43 @@ transit_root_dir=/cygdrive/z/hub/img-tmp
 rsync --info=progress2 -a --remove-source-files "$remote_user@$remote_addr:$remote_root_dir" "$transit_root_dir"
 
 #distribute the images
-find "$transit_root_dir" -type f -print0|while read -d '' -r src_file; do
-	echo -n "$src_file "
+find "$transit_root_dir" -depth -type d -print0|while read -d '' -r src_folder; do
+	dst_folder="$dst_root_dir${src_folder##$transit_root_dir}"
+	dst_folder="${dst_folder/*([^\/]).2chan.net/2chan}"
+	dst_folder="${dst_folder/*([^\/]).@(4chan|4cdn).org/4chan}"
+	dst_folder="${dst_folder/\/src/}"
 
-	#preprocess image
-	if [[ "$src_file" =~ .*\.(jpg|png) ]]; then
-		read format dimensions < <(identify -format '%r %w*%h' "$src_file" 2>/dev/null)
-		if [ $? -eq 0 ]; then
-			size=$(echo "$dimensions"|bc)
-			#check if too small
-			if [[ "$size" -lt "$min_size" ]]; then
-				echo "$dimensions < $min_width*$min_height"
-				rm "$src_file"
-				continue
-			fi
-			#convert certain pngs for ancient versions of acdsee
-			if [[ "$format" == "DirectClassGrayMatte" ]]; then
-				convert "$src_file" -type TrueColorMatte png32:"$src_file"
-				echo -n "converted; ";
+	echo $src_folder $dst_folder
+	mkdir -p "$dst_folder"
+
+	find "$src_folder" -type f -maxdepth 1 -print0|while read -d '' -r src_file; do
+		dst_file="$dst_folder/$(basename "$src_file")"
+
+		echo -n "$src_file "
+
+		#preprocess image
+		if [[ "$src_file" =~ .*\.(jpg|png) ]]; then
+			read format dimensions < <(identify -format '%r %w*%h' "$src_file" 2>/dev/null)
+			if [ $? -eq 0 ]; then
+				size=$(($dimensions))
+
+				#check if too small
+				if [[ "$size" -lt "$min_size" ]]; then
+					echo "$dimensions < $min_width*$min_height"
+					rm "$src_file"
+					continue
+				fi
+
+				#convert certain pngs for ancient versions of acdsee
+				if [[ "$format" == "DirectClassGrayMatte" ]]; then
+					convert "$src_file" -type TrueColorMatte png32:"$src_file"
+					echo -n "converted; "
+				fi
 			fi
 		fi
-	fi
 
-	dst_file="$dst_root_dir${src_file##$transit_root_dir}"
-	dst_file="$(echo $dst_file|sed -e 's/[^/]*\.2chan\.net/2chan/;s/\w\+\.\(4chan\|4cdn\)\.org/4chan/;s/\/src//')"
-	dst_dir="$(dirname "$dst_file")"
-	mkdir -p "$dst_dir"
-	mv "$src_file" "$dst_file" && echo 'ok'
+		mv "$src_file" "$dst_file" && echo 'ok'
+	done
 done
 
 #remove empty directores
