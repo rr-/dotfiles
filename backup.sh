46bbcfd419ddaf730f4190ef98c9bbba3bb39d96
Marcin Kurczewski <mkurczew@gmail.com>
2014-11-16 15:42:09 +0100

    Merged duplicated backup code

--- backup.sh
+++ backup.sh
@@ -12,35 +12,34 @@ server_backup=( /home/backup/backup-sql /etc "/home/$user" /home/srv )
 desktop_backup=( software/utilities clutter img text src )
 transit_dir=/cygdrive/z/hub/backup-tmp
 
+function sync {
+	rsync -T "$transit_dir" \
+		--chmod=D=rwxrxrx,F=rwrr \
+		--info=progress2 \
+		-aKR \
+		--delete-excluded \
+		--delete-during \
+		"$1" \
+		"$2" \
+		--exclude "/cygdrive/z/img/net/" \
+		--exclude "/cygdrive/z/software/utilities/cygwin" \
+		--exclude "/home/$user/img/" \
+		--exclude "/home/srv/www/mal-dev/data/" \
+		--exclude "/home/srv/www/booru-dev/public_html/data/"
+}
+
 mkdir -p "$transit_dir"
 chmod 0777 "$transit_dir"
 
 for x in ${server_backup[@]}; do
 	echo "$server --> $desktop: $x"
-	rsync -T "$transit_dir" \
-		--chmod=D=rwrxrxrx,F=rwrr \
-		--info=progress2 \
-		-aKR \
-		--delete-during \
-		"$user@$server_addr:$x/" \
-		"/cygdrive/z/backup-$server/" \
-		--exclude "/home/$user/img/" \
-		--exclude "/home/srv/www/mal-dev/data/" \
-		--exclude "/home/srv/www/booru-dev/public_html/data/"
+	sync "$user@$server_addr:$x/" "/cygdrive/z/backup-$server/"
 	echo
 done
 
 for x in ${desktop_backup[@]}; do
 	echo "$desktop --> $server: $x"
-	rsync -T "$transit_dir" \
-		--chmod=D=rwxrxrx,F=rwrr \
-		--info=progress2 \
-		-aKR \
-		--delete-during \
-		"/cygdrive/z/$x/" \
-		"$backup_user@$server_addr:/home/backup/backup-$desktop/" \
-		--exclude "/cygdrive/z/img/net/" \
-		--exclude "/cygdrive/z/software/utilities/cygwin"
+	sync "/cygdrive/z/$x/" "$backup_user@$server_addr:/home/backup/backup-$desktop/"
 	echo
 done
 
