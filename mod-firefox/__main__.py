9264734fb69fb5629827bf2a94e40334bab4889c
rr- <mkurczew@gmail.com>
2015-07-06 23:25:26 +0200

    Added experimental module-based installation

---
+++ mod-firefox/__main__.py
@@ -0,0 +1,10 @@
+#!/bin/python
+import os, sys
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.install('firefox')
+if 'cygwin' in sys.platform:
+    FileInstaller.copy_file(os.path.join(dir, '.vimperatorrc'), '~/')
+else:
+    FileInstaller.create_symlink(os.path.join(dir, '.vimperatorrc'), '~/')
