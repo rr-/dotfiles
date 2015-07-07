1ee4ed7d63adf79d1f5562d87166a6260d79da22
rr- <mkurczew@gmail.com>
2015-07-07 09:22:25 +0200

    Added fallback error handling to package installer

--- mod-git/__main__.py
+++ mod-git/__main__.py
@@ -3,6 +3,6 @@ import os
 from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
-PackageInstaller.install('git')
+PackageInstaller.try_install('git')
 FileInstaller.create_symlink(os.path.join(dir, '.gitconfig'), '~/')
 FileInstaller.create_symlink(os.path.join(dir, '.gitignore'), '~/')
