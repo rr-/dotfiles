64b8f17063c2d2e8b980a8f9087176f5e6c32b33
Dash <dash@bad.pink>
2015-07-07 18:29:28 +0200

    Changed bspwm install method to new style

---
+++ mod-bspwm/__main__.py
@@ -0,0 +1,19 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('dmenu')            # program executor
+PackageInstaller.try_install('bspwm-git')        # window manager
+PackageInstaller.try_install('sxhkd-git')        # hotkey manager
+PackageInstaller.try_install('lemonbar-xft-git') # panel
+PackageInstaller.try_install('feh')              # wallpaper renderer
+
+PackageInstaller.try_install('psutil', method='pip')      # CPU usage
+PackageInstaller.try_install('pyalsaaudio', method='pip') # system volume
+PackageInstaller.try_install('python-mpd2', method='pip') # mpd interaction
+
+FileInstaller.create_symlink(os.path.join(dir, '.sxhkdrc'), '~/.config/sxhkd/sxhkdrc')
+FileInstaller.create_symlink(os.path.join(dir, '.bspwmrc'), '~/.config/bspwm/bspwmrc')
+FileInstaller.create_symlink(os.path.join(dir, 'panel'), '~/.config/bspwm/panel')
+FileInstaller.create_symlink(os.path.join(dir, '.xinitrc'), '~/')
