c5cfcb2e7f35525d15d8772b7ae9dc39bb01c5d4
Dash <dash@bad.pink>
2015-07-07 22:17:32 +0200

    Changed zsh install method to new style

---
+++ mod-zsh/__main__.py
@@ -0,0 +1,7 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('zsh')
+FileInstaller.create_symlink(os.path.join(dir, '.zshrc'), '~/')
