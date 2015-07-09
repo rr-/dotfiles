11d64894e438055d94e13c987e1aa30edc3a8331
Dash <dash@bad.pink>
2015-07-09 09:31:43 +0200

    Fixed lightness switcher for MinTTY

--- mod-term-mintty/__main__.py
+++ mod-term-mintty/__main__.py
@@ -4,4 +4,6 @@ from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
 FileInstaller.create_symlink(os.path.join(dir, '.inputrc'), '~/')
+FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-dark'), '~/.minttyrc-dark')
+FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-light'), '~/.minttyrc-light')
 FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-light'), '~/.minttyrc')
