d5524035992c4e064c7c12e2e38e676ee761f9f3
Dash <dash@bad.pink>
2015-07-11 10:54:54 +0200

    Added CPU usage chart

--- mod-bspwm/widgets/cpu.py
+++ mod-bspwm/widgets/cpu.py
@@ -1,16 +1,22 @@
 import psutil
 from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+from .chart import Chart
 
 class CpuProvider(object):
     delay = 0
 
     def __init__(self, main_window):
         self.label = QtGui.QLabel()
+        self.chart = Chart(QtCore.QSize(80, main_window.height()))
         main_window[0].right_widget.layout().addWidget(self.label)
+        main_window[0].right_widget.layout().addWidget(self.chart)
 
     def refresh(self):
         self.percentage = psutil.cpu_percent(interval=1)
 
     def render(self):
-        self.label.setText('CPU: %5.02f%%' % (self.percentage))
-
+        self.label.setText('CPU %.1f%%' % (self.percentage))
+        self.chart.addPoint('#ff0000', self.percentage)
+        self.chart.repaint()
