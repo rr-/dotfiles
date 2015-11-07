b1d2c25cd1cf3c915cafa11e4598234e6f93a8c1
rr- <mkurczew@gmail.com>
2015-11-07 09:37:38 +0000

    mod-tool-eyecandy: correct dependencies

--- mod-tool-eyecandy/__main__.py
+++ mod-tool-eyecandy/__main__.py
@@ -3,5 +3,7 @@ import os
 from libinstall import PackageInstaller, FileInstaller
 dir = os.path.dirname(__file__)
 
+PackageInstaller.try_install('python-pyqt4')
+PackageInstaller.try_install('phonon-qt4')
 PackageInstaller.try_install('eyecandy', method='pip')
 FileInstaller.create_symlink(os.path.join(dir, 'candy.json'), '~/.config/EyeCandy/candy.json')
