597380661664544ca5e1ce971fabb20b25755063
rr- <rr-@sakuya.pl>
2015-11-25 13:09:55 +0100

    don't use dots in dotfiles
    So that they're actually visible in the file browsers.

--- mod-term-mintty/__main__.py
+++ mod-term-mintty/__main__.py
@@ -3,7 +3,7 @@ import os
 from libinstall import FileInstaller, PackageInstaller
 dir = os.path.dirname(__file__)
 
-FileInstaller.create_symlink(os.path.join(dir, '.inputrc'), '~/')
-FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-dark'), '~/.minttyrc-dark')
-FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-light'), '~/.minttyrc-light')
-FileInstaller.create_symlink(os.path.join(dir, '.minttyrc-light'), '~/.minttyrc')
+FileInstaller.create_symlink(os.path.join(dir, 'inputrc'), '~/.inputrc')
+FileInstaller.create_symlink(os.path.join(dir, 'minttyrc-dark'), '~/.minttyrc-dark')
+FileInstaller.create_symlink(os.path.join(dir, 'minttyrc-light'), '~/.minttyrc-light')
+FileInstaller.create_symlink(os.path.join(dir, 'minttyrc-light'), '~/.minttyrc')
