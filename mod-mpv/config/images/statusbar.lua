5adee6ca267d1a84381aa3d1a691e86f72dfdc01
Dash <dash@bad.pink>
2016-03-13 11:13:11 +0100

    mod-mpv: replace eyecandy with mpv (as "mpi")

---
+++ mod-mpv/config/images/statusbar.lua
@@ -0,0 +1,30 @@
+local on = true
+
+function update_statusbar()
+    if on == true then
+        local playlist_pos = tonumber(mp.get_property('playlist-pos')) + 1
+        local playlist_count = tonumber(mp.get_property('playlist-count'))
+        local path = mp.get_property('filename')
+        mp.set_property(
+            'options/osd-msg1',
+            string.format(
+                '%s (%d/%d)',
+                path,
+                playlist_pos,
+                playlist_count))
+    end
+end
+
+function toggle_statusbar()
+    if on == false then
+        on = true
+        update_statusbar()
+    else
+        on = false
+        mp.set_property('options/osd-msg1', '')
+    end
+    mp.resume_all()
+end
+
+mp.register_event('file-loaded', update_statusbar)
+mp.register_script_message('toggle-statusbar', toggle_statusbar)
--- mod-tool-eyecandy/__main__.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-import os
-from libinstall import PackageInstaller, FileInstaller
-dir = os.path.dirname(__file__)
-
-PackageInstaller.try_install('python-pyqt4')
-PackageInstaller.try_install('phonon-qt4')
-PackageInstaller.try_install('eyecandy', method='pip')
-FileInstaller.create_symlink(os.path.join(dir, 'candy.json'), '~/.config/EyeCandy/candy.json')
