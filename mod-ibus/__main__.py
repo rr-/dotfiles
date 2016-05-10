00d5ba0740cdba50242b038f123a8dc71c8d61ec
rr- <rr-@sakuya.pl>
2016-05-10 17:05:59 +0200

    mod-ibus: sandbox whole thing, provide gtk3 theme

---
+++ mod-ibus/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+from libinstall import FileInstaller, PackageInstaller
+dir = os.path.dirname(__file__)
+
+PackageInstaller.try_install('ibus')
+PackageInstaller.try_install('ibus-anthy')
+
+FileInstaller.create_symlink(os.path.join(dir, 'gtk-settings.ini'), '~/.config/ibus/gtk-3.0/settings.ini')
+FileInstaller.create_symlink(os.path.join(dir, 'start'), '~/.config/ibus/start')
+FileInstaller.create_symlink(os.path.join(dir, 'setup'), '~/.config/ibus/setup')
+FileInstaller.create_dir('~/.config/ibus/dconf')
+FileInstaller.create_dir('~/.config/ibus/ibus')
+FileInstaller.create_dir('~/.config/ibus/ibus-anthy')
