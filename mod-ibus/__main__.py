ef52ffe254facc08174b02485d6f4f135941f1b3
Dash <dash@bad.pink>
2016-05-11 00:25:47 +0200

    mod-ibus: remove sandboxing, add GTK3 CSS approach

--- mod-ibus/__main__.py
+++ mod-ibus/__main__.py
@@ -6,13 +6,6 @@ dir = os.path.dirname(__file__)
 PackageInstaller.try_install('ibus')
 PackageInstaller.try_install('ibus-anthy')
 
-FileInstaller.create_symlink(os.path.join(dir, 'gtk-settings.ini'), '~/.config/ibus/gtk-3.0/settings.ini')
+FileInstaller.create_symlink(os.path.join(dir, 'gtk.css'), '~/.config/gtk-3.0/gtk.css')
 FileInstaller.create_symlink(os.path.join(dir, 'start'), '~/.config/ibus/start')
 FileInstaller.create_symlink(os.path.join(dir, 'setup'), '~/.config/ibus/setup')
-FileInstaller.create_dir('~/.config/ibus/dconf')
-FileInstaller.create_dir('~/.config/ibus/ibus')
-FileInstaller.create_dir('~/.config/ibus/ibus-anthy')
-
-# TODO: try to symlink dconf/user to ibus/dconf/user (NOT the other way around)
-# if this doesn't work, remember to copy dconf/user to ibus/dconf/user after each change,
-# or just commit it.
