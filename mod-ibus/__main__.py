0f36ccabfc2ddad2e0290da345ded00935b99864
Dash <dash@bad.pink>
2016-05-10 18:27:28 +0200

    mod-ibus: add note to future self

--- mod-ibus/__main__.py
+++ mod-ibus/__main__.py
@@ -12,3 +12,7 @@ FileInstaller.create_symlink(os.path.join(dir, 'setup'), '~/.config/ibus/setup')
 FileInstaller.create_dir('~/.config/ibus/dconf')
 FileInstaller.create_dir('~/.config/ibus/ibus')
 FileInstaller.create_dir('~/.config/ibus/ibus-anthy')
+
+# TODO: try to symlink dconf/user to ibus/dconf/user (NOT the other way around)
+# if this doesn't work, remember to copy dconf/user to ibus/dconf/user after each change,
+# or just commit it.
