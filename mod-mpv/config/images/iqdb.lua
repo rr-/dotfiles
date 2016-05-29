63bfec6cece5cf8b43811cad4002b12f6cb4e366
Dash <dash@bad.pink>
2016-05-29 20:42:57 +0200

    mod-mpv: add IQDB integration

---
+++ mod-mpv/config/images/iqdb.lua
@@ -0,0 +1,25 @@
+local mp_utils = require('mp.utils')
+
+function search_iqdb()
+    name = os.tmpname()
+    handle = io.open(name, 'w')
+
+    mp.osd_message('Downloading results from IQDB...')
+    mp_utils.subprocess({args = {
+        'curl',
+        '-F', 'file=@' .. mp.get_property('path'),
+        '-o', name,
+        'https://iqdb.org/',
+    }})
+
+    mp_utils.subprocess({args = {
+        'sed',
+        '-i', 's#</head>#<base href="https://iqdb.org">$0#',
+        name}
+    })
+
+    mp_utils.subprocess({args = {
+        'firefox', name}})
+end
+
+mp.register_script_message('search-iqdb', search_iqdb)
