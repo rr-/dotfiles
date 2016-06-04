1be351a3a94d4cc9d5a416c549b5fee78230b832
rr- <rr-@sakuya.pl>
2016-06-04 20:32:19 +0200

    mod-nvim: try out nvim and fzf
    Also, add """plugin""" system for zsh

---
+++ mod-nvim/__main__.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import os
+from libinstall import FileInstaller, PackageInstaller, run_verbose
+dir = os.path.dirname(__file__)
+spell_dir = os.path.expanduser('~/.config/nvim/spell/')
+
+PackageInstaller.try_install('neovim-git')
+PackageInstaller.try_install('vim-fzf')
+
+for folder in ['undo', 'backup', 'swap', 'spell', 'autoload']:
+    FileInstaller.create_dir('~/.config/nvim/' + folder)
+
+FileInstaller.create_symlink(os.path.join(dir, '../mod-vim/spell/pl.utf-8.add'), spell_dir)
+FileInstaller.create_symlink(os.path.join(dir, '../mod-vim/spell/en.utf-8.add'), spell_dir)
+FileInstaller.create_symlink(os.path.join(dir, 'vim-plug/plug.vim'), '~/.config/nvim/autoload/plug.vim')
+FileInstaller.create_symlink(os.path.join(dir, 'init.vim'), '~/.config/nvim/init.vim')
+FileInstaller.download('ftp://ftp.vim.org/pub/vim/runtime/spell/en.utf-8.spl', '~/.config/nvim/spell/')
+FileInstaller.download('ftp://ftp.vim.org/pub/vim/runtime/spell/pl.utf-8.spl', '~/.config/nvim/spell/')
+FileInstaller.create_file('~/.config/zsh/editor.sh', 'export EDITOR=vim;alias vim=nvim', overwrite=True)
+
+for fn in os.listdir(spell_dir):
+    if 'add' in fn and not 'spl' in fn:
+        run_verbose(['nvim', '-c', 'mkspell! ' + os.path.join(spell_dir, fn) + '|quit'])
+
+if FileInstaller.has_executable('devenv'):
+    FileInstaller.copy_file(os.path.join(dir, 'vimrc'), '~/.vsvimrc')
