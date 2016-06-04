81ed20fc518f9f8638b80e60b0daf82d3a333c2f
Dash <dash@bad.pink>
2016-06-04 20:58:45 +0200

    mod-ibus: remove gtk stylesheet (ibus/ibus#1856)

--- mod-ibus/__main__.py
+++ mod-ibus/__main__.py
@@ -6,6 +6,5 @@ dir = os.path.dirname(__file__)
 PackageInstaller.try_install('ibus')
 PackageInstaller.try_install('ibus-anthy')
 
-FileInstaller.create_symlink(os.path.join(dir, 'gtk.css'), '~/.config/gtk-3.0/gtk.css')
 FileInstaller.create_symlink(os.path.join(dir, 'start'), '~/.config/ibus/start')
 FileInstaller.create_symlink(os.path.join(dir, 'setup'), '~/.config/ibus/setup')
