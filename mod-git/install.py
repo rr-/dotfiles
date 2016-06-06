8899a2b2256e4d422c28cf21545fc872f492b9ad
Dash <dash@bad.pink>
2016-06-07 00:21:10 +0200

    mod-git: add git extras
    https://github.com/tj/git-extras this specifically.

--- mod-git/install.py
+++ mod-git/install.py
@@ -2,5 +2,6 @@ import packages
 import util
 
 packages.try_install('git')
+packages.try_install('git-extras-git')
 util.create_symlink('./gitconfig', '~/.gitconfig')
 util.create_symlink('./gitignore', '~/.gitignore')
