c830c08bf42a954915cb38eb9125b5a085b9a3bc
Dash <dash@bad.pink>
2016-06-07 13:02:31 +0200

    mod-tools-search: add 'lgrep' zsh alias

--- mod-tools-search/install.py
+++ mod-tools-search/install.py
@@ -4,3 +4,4 @@ import util
 packages.try_install('fzf') # super opener
 packages.try_install('silver-searcher-git') # super grep
 util.create_symlink('./agignore', '~/.agignore')
+util.create_file('~/.config/zsh/lgrep.sh', 'lgrep() { ag "$@" | fzf }')
