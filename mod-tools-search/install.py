db00ad59dfe1f4ae3917c0a81a479fa3a452606c
rr- <rr-@sakuya.pl>
2016-06-07 13:02:23 +0200

    mod-tools-search: add agignore

--- mod-tools-search/install.py
+++ mod-tools-search/install.py
@@ -1,4 +1,6 @@
 import packages
+import util
 
 packages.try_install('fzf') # super opener
 packages.try_install('silver-searcher-git') # super grep
+util.create_symlink('./agignore', '~/.agignore')
