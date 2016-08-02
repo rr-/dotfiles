667f156b63e080850fca1023f0c01fae5ce28f1a
rr- <rr-@sakuya.pl>
2016-08-02 19:11:07 +0200

    mod-xorg: don't hardcode startup scripts

--- mod-mpd/install.py
+++ mod-mpd/install.py
@@ -5,7 +5,8 @@ if not packages.try_install('mpd-light'):
     packages.try_install('mpd')
 packages.try_install('mpc')
 
-util.create_symlink('./config', '~/.config/mpd')
 util.create_dir('~/.config/mpd/playlists')
 for file in ['database', 'log', 'pid', 'state', 'sticker.sql']:
     util.create_file('~/.config/mpd/' + file)
+util.create_symlink('./config', '~/.config/mpd')
+util.create_symlink('./start', '~/.config/x/start-mpd.sh')
