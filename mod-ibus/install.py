69cfb945997c2420035358d3b9d77de6fa326b63
Dash <dash@bad.pink>
2016-08-02 19:11:07 +0200

    mod-xorg: don't hardcode startup scripts

--- mod-ibus/install.py
+++ mod-ibus/install.py
@@ -3,5 +3,6 @@ import util
 
 packages.try_install('ibus')
 packages.try_install('ibus-anthy')
+util.create_symlink('./start', '~/.config/x/start-ibus.sh')
 util.create_symlink('./start', '~/.config/ibus/start')
 util.create_symlink('./setup', '~/.config/ibus/setup')
