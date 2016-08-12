213d5e10f0b7b67807afb4023f0d152f8d1eb335
Dash <dash@bad.pink>
2016-08-12 21:43:06 +0200

    mod-mpv: fix fancy zoom breaking on certain files

--- mod-mpv/config/images/fancy_zoom.lua
+++ mod-mpv/config/images/fancy_zoom.lua
@@ -4,16 +4,8 @@ local user_fiddled = false
 
 math.sign = math.sign or function(x) return x<0 and -1 or x>0 and 1 or 0 end
 
-function get(property)
-    return mp.get_property(property)
-end
-
-function set(property, value)
-    mp.set_property(property, value)
-end
-
-function add(property, delta)
-    set(property, get(property) + delta)
+function add_property(property, delta)
+    mp.set_property_number(property, mp.get_property_number(property) + delta)
 end
 
 function add_rotation(delta)
@@ -29,18 +21,16 @@ end
 
 function get_real_scale()
     local video = {
-        width  = get('video-params/dw'),
-        height = get('video-params/dh'),
+        width  = mp.get_property_number('video-params/dw', nil),
+        height = mp.get_property_number('video-params/dh', nil),
     }
     local window = {
-        width  = get('osd-width'),
-        height = get('osd-height'),
+        width  = mp.get_property_number('osd-width', nil),
+        height = mp.get_property_number('osd-height', nil),
     }
-    if video.width == nil or window.width == nil then return nil end
-    video.width  = tonumber(video.width)
-    video.height = tonumber(video.height)
-    window.width  = tonumber(window.width)
-    window.height = tonumber(window.height)
+    if video.width == nil or window.width == nil then
+        return nil
+    end
     if window.width / window.height < video.width / video.height then
         return window.width / video.width
     else
@@ -49,30 +39,34 @@ function get_real_scale()
 end
 
 function reset_other_settings()
-    set('video-pan-x', 0)
-    set('video-pan-y', 0)
-    set('video-aspect', 0)
+    mp.set_property_number('video-pan-x', 0)
+    mp.set_property_number('video-pan-y', 0)
+    mp.set_property_number('video-aspect', 0)
 end
 
 function correct_pan_for_dimension(dimension, delta)
-    value = tonumber(get('video-pan-' .. dimension))
+    value = mp.get_property_number('video-pan-' .. dimension)
     if dimension == 'x' then
-        window_size = tonumber(get('osd-width'))
-        video_size = tonumber(get('video-params/dw'))
+        window_size = mp.get_property_number('osd-width', nil)
+        video_size = mp.get_property_number('video-params/dw', nil)
     else
-        window_size = tonumber(get('osd-height'))
-        video_size = tonumber(get('video-params/dh'))
+        window_size = mp.get_property_number('osd-height', nil)
+        video_size = mp.get_property_number('video-params/dh', nil)
     end
     window_scale = get_real_scale()
-    zoom_scale = tonumber(get('video-zoom')) + 1
+    zoom_scale = mp.get_property_number('video-zoom') + 1
+
+    if window_size == nil or video_size == nil or window_scale == nil then
+        return
+    end
 
     -- what a bunch of crap
     actual_canvas_size = video_size * window_scale * zoom_scale
     if actual_canvas_size < window_size then
-        set('video-pan-' .. dimension, 0)
+        mp.set_property_number('video-pan-' .. dimension, 0)
     elseif (1 - math.abs(value) * 2) * actual_canvas_size < window_size then
         value = math.sign(value) * (-(window_size / actual_canvas_size - 1) / 2)
-        set('video-pan-' .. dimension, value)
+        mp.set_property_number('video-pan-' .. dimension, value)
     end
 end
 
@@ -86,7 +80,7 @@ function scale_original()
     if window_scale == nil then return end
     local target_scale = (1 / window_scale) - 1
     reset_other_settings()
-    set('video-zoom', target_scale)
+    mp.set_property_number('video-zoom', target_scale)
     correct_pan()
 end
 
@@ -95,13 +89,13 @@ function scale_arbitrary(multiplier)
     if window_scale == nil then return end
     local target_scale = ((1 / window_scale * multiplier) - 1)
     reset_other_settings()
-    set('video-zoom', target_scale)
+    mp.set_property_number('video-zoom', target_scale)
     correct_pan()
 end
 
 function scale_to_window()
     reset_other_settings()
-    set('video-zoom', 0)
+    mp.set_property_number('video-zoom', 0)
     correct_pan()
 end
 
@@ -141,9 +135,9 @@ mp.register_event('video-reconfig', window_size_changed)
 mp.register_script_message('fit-to-window', function() fit_to_window(); user_fiddled = true; end)
 mp.register_script_message('fit-original',  function() scale_original(); user_fiddled = true; end)
 mp.register_script_message('set-zoom',      function(v) scale_arbitrary(v); user_fiddled = true; end)
-mp.register_script_message('zoom-out',      function(d) add('video-zoom', -d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('zoom-in',       function(d) add('video-zoom', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('pan-x',         function(d) add('video-pan-x', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('pan-y',         function(d) add('video-pan-y', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('aspect',        function(d) add('video-aspect', d); correct_pan(); user_fiddled = true; end)
+mp.register_script_message('zoom-out',      function(d) add_property('video-zoom', -d); correct_pan(); user_fiddled = true; end)
+mp.register_script_message('zoom-in',       function(d) add_property('video-zoom', d); correct_pan(); user_fiddled = true; end)
+mp.register_script_message('pan-x',         function(d) add_property('video-pan-x', d); correct_pan(); user_fiddled = true; end)
+mp.register_script_message('pan-y',         function(d) add_property('video-pan-y', d); correct_pan(); user_fiddled = true; end)
+mp.register_script_message('aspect',        function(d) add_property('video-aspect', d); correct_pan(); user_fiddled = true; end)
 mp.register_script_message('rotate',        function(d) add_rotation(d); user_fiddled = true; end)
