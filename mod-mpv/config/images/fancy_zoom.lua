87f1ba1cd2fe08e945e60f80cd9e04e2fbcc0c7c
rr- <rr-@sakuya.pl>
2016-08-13 00:07:00 +0200

    mod-mpv: simplify fancy zoom
    In particular, don't rely on my custom patch that adds on-prerender
    hook.
    
    This has two side effects:
    
    - everything is more performant
    - images are fit to window rather than being displayed at their physical
      resolutions. (In other words, small images are upscaled.) Test drive
      on 1000 images shows I can get used to it.

--- mod-mpv/config/images/fancy_zoom.lua
+++ mod-mpv/config/images/fancy_zoom.lua
@@ -1,6 +1,4 @@
 local rotation = 0
-local rendering = false
-local user_fiddled = false
 
 math.sign = math.sign or function(x) return x<0 and -1 or x>0 and 1 or 0 end
 
@@ -38,12 +36,6 @@ function get_real_scale()
     end
 end
 
-function reset_other_settings()
-    mp.set_property_number('video-pan-x', 0)
-    mp.set_property_number('video-pan-y', 0)
-    mp.set_property_number('video-aspect', 0)
-end
-
 function correct_pan_for_dimension(dimension, delta)
     value = mp.get_property_number('video-pan-' .. dimension)
     if dimension == 'x' then
@@ -79,24 +71,18 @@ function scale_original()
     local window_scale = get_real_scale()
     if window_scale == nil then return end
     local target_scale = (1 / window_scale) - 1
-    reset_other_settings()
     mp.set_property_number('video-zoom', target_scale)
-    correct_pan()
 end
 
 function scale_arbitrary(multiplier)
     local window_scale = get_real_scale()
     if window_scale == nil then return end
     local target_scale = ((1 / window_scale * multiplier) - 1)
-    reset_other_settings()
     mp.set_property_number('video-zoom', target_scale)
-    correct_pan()
 end
 
 function scale_to_window()
-    reset_other_settings()
     mp.set_property_number('video-zoom', 0)
-    correct_pan()
 end
 
 function fit_to_window()
@@ -109,35 +95,38 @@ function fit_to_window()
     end
 end
 
-function file_changed()
-    user_fiddled = false
-    rendering = true
+function reset_pan()
+    mp.set_property_number('video-pan-x', 0)
+    mp.set_property_number('video-pan-y', 0)
 end
 
-function file_rendered()
-    -- https://github.com/mpv-player/mpv/pull/2936
-    if rendering then
-        rendering = false
-        fit_to_window()
-    end
+function reset_zoom()
+    mp.set_property_number('video-zoom', 0)
+end
+
+function reset_rotation()
+    mp.command('no-osd vf set ""')
+end
+
+function file_changed()
+    reset_pan()
+    reset_zoom()
+    reset_rotation()
 end
 
 function window_size_changed()
-    if not rendering and not user_fiddled then
+    if not user_fiddled then
         fit_to_window()
     end
 end
 
-mp.add_hook('on_prerender', 50, file_rendered)
-mp.observe_property('osd-width', int, window_size_changed)
-mp.register_event('file-loaded', file_changed)
-mp.register_event('video-reconfig', window_size_changed)
-mp.register_script_message('fit-to-window', function() fit_to_window(); user_fiddled = true; end)
-mp.register_script_message('fit-original',  function() scale_original(); user_fiddled = true; end)
-mp.register_script_message('set-zoom',      function(v) scale_arbitrary(v); user_fiddled = true; end)
-mp.register_script_message('zoom-out',      function(d) add_property('video-zoom', -d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('zoom-in',       function(d) add_property('video-zoom', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('pan-x',         function(d) add_property('video-pan-x', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('pan-y',         function(d) add_property('video-pan-y', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('aspect',        function(d) add_property('video-aspect', d); correct_pan(); user_fiddled = true; end)
-mp.register_script_message('rotate',        function(d) add_rotation(d); user_fiddled = true; end)
+mp.register_event('end-file', file_changed)
+mp.register_script_message('scale-original', function() reset_pan(); scale_original(); end)
+mp.register_script_message('fit-to-window',  function() reset_pan(); reset_zoom(); end)
+mp.register_script_message('set-zoom',       function(v) reset_pan(); scale_arbitrary(v); end)
+mp.register_script_message('zoom-out',       function(d) add_property('video-zoom', -d); correct_pan(); end)
+mp.register_script_message('zoom-in',        function(d) add_property('video-zoom', d); correct_pan(); end)
+mp.register_script_message('pan-x',          function(d) add_property('video-pan-x', d); correct_pan(); end)
+mp.register_script_message('pan-y',          function(d) add_property('video-pan-y', d); correct_pan(); end)
+mp.register_script_message('aspect',         function(d) add_property('video-aspect', d); correct_pan(); end)
+mp.register_script_message('rotate',         function(d) add_rotation(d); end)
