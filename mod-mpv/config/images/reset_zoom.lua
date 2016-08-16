b2e57eb4ebb04e1219a92e45e8b28faf2af928f4
rr- <rr-@sakuya.pl>
2016-08-16 10:25:58 +0200

    mod-mpv/fancy-zoom: drop fancy zoom

---
+++ mod-mpv/config/images/reset_zoom.lua
@@ -0,0 +1,9 @@
+function file_changed()
+    mp.set_property('video-unscaled', 'no')
+    mp.set_property_number('video-pan-x', 0)
+    mp.set_property_number('video-pan-y', 0)
+    mp.set_property_number('video-zoom', 0)
+    mp.set_property_number('video-rotate', 0)
+end
+
+mp.register_event('end-file', file_changed)
