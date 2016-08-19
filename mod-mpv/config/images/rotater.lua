c48847742bc77b34c36e6ffc4948c8ca8dfd1e1d
Dash <dash@bad.pink>
2016-08-19 09:52:43 +0200

    mod-mpv: use arbitrary rotations
    Thanks, mpv-player/mpv#3434

---
+++ mod-mpv/config/images/rotater.lua
@@ -0,0 +1,9 @@
+function rotate(number)
+    rotation = mp.get_property_number('video-rotate')
+    rotation = rotation + number
+    rotation = rotation + 360
+    rotation = rotation % 360
+    mp.set_property('video-rotate', rotation)
+end
+
+mp.register_script_message('rotate', rotate)
