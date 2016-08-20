793121d24c9b4c916b7885a6a009185b3ba3a291
rr- <rr-@sakuya.pl>
2016-08-20 12:48:39 +0200

    mod-mpv: reset zoom only for images

--- mod-mpv/config/scripts/reset_zoom.lua
+++ mod-mpv/config/scripts/reset_zoom.lua
@@ -1,9 +1,13 @@
+local on = mp.get_opt('reset-zoom') == 'yes'
+
 function file_changed()
-    mp.set_property('video-unscaled', 'no')
-    mp.set_property_number('video-pan-x', 0)
-    mp.set_property_number('video-pan-y', 0)
-    mp.set_property_number('video-zoom', 0)
-    mp.set_property_number('video-rotate', 0)
+    if on == true then
+        mp.set_property('video-unscaled', 'no')
+        mp.set_property_number('video-pan-x', 0)
+        mp.set_property_number('video-pan-y', 0)
+        mp.set_property_number('video-zoom', 0)
+        mp.set_property_number('video-rotate', 0)
+    end
 end
 
 mp.register_event('end-file', file_changed)
