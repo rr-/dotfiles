64c506a97c6d66c8e4a0f3c6b768250c5da73caf
rr- <rr-@sakuya.pl>
2016-08-25 13:16:26 +0200

    scripts/tagger: add standalone interactive tagger
    To be used with send-to-yume and send-to-gelbooru

---
+++ lib/dotfiles/tag_list.py
@@ -0,0 +1,29 @@
+class TagList():
+    def __init__(self):
+        self._chosen_tags = []
+
+    def is_tagged(self, word):
+        return word.lower() in [tag.lower() for tag in self._chosen_tags]
+
+    def add(self, tag):
+        tag = tag.strip()
+        if not tag:
+            return
+        if self.is_tagged(tag):
+            return
+        self._chosen_tags.append(tag)
+
+    def add_all(self, collection):
+        for tag in collection:
+            self.add(tag)
+
+    def remove(self, word):
+        self._chosen_tags = [
+            tag for tag in self._chosen_tags if tag.lower() != word.lower()]
+
+    def remove_all(self, collection):
+        for tag in collection:
+            self.remove(tag)
+
+    def get(self):
+        return self._chosen_tags
