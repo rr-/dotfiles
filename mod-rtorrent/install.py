b3f37ce893966b4c55240b7c05eb7ada9e269f02
Dash <dash@bad.pink>
2016-09-02 11:09:46 +0200

    mod-rtorrent: add rtorrent

---
+++ mod-rtorrent/install.py
@@ -0,0 +1,11 @@
+import packages
+import util
+
+packages.try_install('rtorrent')
+
+util.create_dir('~/hub/rtorrent/session')
+util.create_dir('~/hub/rtorrent/data')
+util.create_symlink('./rtorrent.rc', '~/.rtorrent.rc')
+
+# couldn't be bothered to create definition that ACTUALLY FUCKING WORKS
+# util.create_symlink('./rtorrent.service', '~/.config/systemd/user/rtorrent.service')
