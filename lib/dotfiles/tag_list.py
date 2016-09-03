e61310eb3812aee31fb7fd8ac7099f340c5feaee
Dash <dash@bad.pink>
2016-09-03 23:15:35 +0200

    scripts/tagger: add implications support

--- lib/dotfiles/tag_list.py
+++ lib/dotfiles/tag_list.py
@@ -1,13 +1,13 @@
 class TagList():
     def __init__(self):
         self._chosen_tags = []
-        self._index = set()
+        self._index = {}
 
     def add(self, tag):
         if not str(tag).strip() or tag in self:
             return
         self._chosen_tags.append(tag)
-        self._index.add(str(tag).lower())
+        self._index[str(tag).lower()] = tag
 
     def add_all(self, collection):
         for tag in collection:
@@ -17,11 +17,14 @@ class TagList():
         self._chosen_tags = [
             t for t in self._chosen_tags \
                 if str(t).lower() != str(tag).lower()]
-        self._index.remove(str(tag).lower())
+        del self._index[str(tag).lower()]
 
     def remove_all(self, collection):
         for tag in collection:
             self.remove(tag)
+
+    def __getitem__(self, tag):
+        return self._index.get(str(tag).lower(), None)
 
     def __iter__(self):
         return self._chosen_tags.__iter__()
@@ -29,5 +32,5 @@ class TagList():
     def __len__(self):
         return len(self._chosen_tags)
 
-    def __contains__(self, word):
-        return str(word).lower() in self._index
+    def __contains__(self, tag):
+        return str(tag).lower() in self._index
