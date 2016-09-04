e5c2d95f7f54dc3d252128df219d6dd2bce44a34
rr- <rr-@sakuya.pl>
2016-09-04 15:39:38 +0200

    mod-search: drop unused alias

--- mod/search/install.py
+++ mod/search/install.py
@@ -6,4 +6,3 @@ def run():
     packages.try_install('fzf')  # super opener
     packages.try_install('silver-searcher-git')  # super grep
     util.create_symlink('./agignore', '~/.agignore')
-    util.create_file('~/.config/zsh/lgrep.sh', 'lgrep() { ag "$@" | fzf }')
