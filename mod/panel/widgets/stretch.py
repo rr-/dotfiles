e2a3e3efa814f084c6d68985b9c6385c4d46598a
Dash <dash@bad.pink>
2016-10-15 18:10:35 +0200

    mod/panel: extract StretchWidget to separate file

---
+++ mod/panel/widgets/stretch.py
@@ -0,0 +1,11 @@
+class StretchWidget:
+    delay = 1000
+
+    def __init__(self, main_window):
+        main_window[0].layout().addStretch()
+
+    def render(self):
+        pass
+
+    def refresh(self):
+        pass
