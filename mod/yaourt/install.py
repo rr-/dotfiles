762957a92704e8d5e43d63bea6bfce8571fd2509
Dash <dash@bad.pink>
2016-11-27 10:41:26 +0100

    mod/yaourt/install: fix style

--- mod/yaourt/install.py
+++ mod/yaourt/install.py
@@ -15,11 +15,13 @@ def install_aur(package):
     root_dir = '/tmp/install-' + package
     os.makedirs(root_dir, exist_ok=True)
     os.chdir(root_dir)
-    url = ('https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=%s' %
+    url = (
+        'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=%s' %
         package)
     util.download(url, os.path.join(root_dir, 'PKGBUILD'))
     util.run_verbose(['makepkg', 'PKGBUILD', '--skippgpcheck'])
-    util.run_verbose(['sudo', '-S', 'pacman', '-U', '--noconfirm',
+    util.run_verbose([
+        'sudo', '-S', 'pacman', '-U', '--noconfirm',
         util.find(os.path.join(root_dir, '*.tar.xz'))[0]])
     shutil.rmtree(root_dir)
     os.chdir(old_dir)
@@ -28,14 +30,14 @@ def install_aur(package):
 def run():
     util.run_verbose(['sudo', '-S', 'pacman', '-Syu'])
     packages.install('sudo')
-    packages.install('base-devel') # package-query
-    packages.install('tar')
-    packages.install('wget')
-    packages.install('gcc')       # to compile everything
-    packages.install('make')      # to compile everything
-    packages.install('fakeroot')  # for package-query
-    packages.install('yajl')      # for package-query
-    packages.install('binutils')  # for yaourt
+    packages.install('base-devel')  # package-query
+    packages.install('tar')         # to unpack the package
+    packages.install('wget')        # to download the package
+    packages.install('gcc')         # to compile everything
+    packages.install('make')        # to compile everything
+    packages.install('fakeroot')    # for package-query
+    packages.install('yajl')        # for package-query
+    packages.install('binutils')    # for yaourt
 
     install_aur('package-query')
     install_aur('yaourt')
