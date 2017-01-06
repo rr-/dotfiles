b4c9e9249035ef8ba12222fe51bee58b99043df6
Dash <dash@bad.pink>
2017-01-06 16:49:10 +0100

    stay on the light side

--- mod/term/mintty/install.py
+++ mod/term/mintty/install.py
@@ -3,6 +3,4 @@ import util
 
 def run():
     util.create_symlink('./inputrc', '~/.inputrc')
-    util.create_symlink('./minttyrc-dark', '~/.minttyrc-dark')
-    util.create_symlink('./minttyrc-light', '~/.minttyrc-light')
-    util.create_symlink('./minttyrc-light', '~/.minttyrc')
+    util.create_symlink('./minttyrc', '~/.minttyrc')
