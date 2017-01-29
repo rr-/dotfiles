8c2536e872dac9485e5ce0fea2b9a756d652ea8b
rr- <rr-@sakuya.pl>
2017-01-29 14:58:40 +0100

    mod/term/xterm: make installable

--- mod/term/xterm/install.py
+++ mod/term/xterm/install.py
@@ -1,2 +1,10 @@
+from os.path import expanduser
+from dotinstall import packages
+from dotinstall import util
+
+
 def run():
-    raise NotImplementedError()
+    packages.try_install('xterm')
+
+    util.create_symlink('./Xresources', '~/.config/Xresources')
+    util.run_verbose(['xrdb', '-override', expanduser('~/.config/Xresources')])
