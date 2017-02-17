1302367ee1baeae85e59ff19cf7cda6c9553f98b
rr- <rr-@sakuya.pl>
2017-02-17 18:23:45 +0100

    mod/mutt: render html e-mails

--- mod/mutt/install.py
+++ mod/mutt/install.py
@@ -4,9 +4,11 @@ from dotinstall import util
 
 def run():
     packages.try_install('neomutt')
+    packages.try_install('w3m')
     util.create_file('~/.mutt/certificates')
     util.create_dir('~/.mutt/cache/bodies')
     util.create_dir('~/.mutt/cache/headers')
     util.create_symlink('./file_email', '~/.mutt/')
     util.create_symlink('./colors.muttrc', '~/.mutt/')
     util.create_symlink('./muttrc', '~/.mutt/')
+    util.create_symlink('./mailcap', '~/.mailcap')
