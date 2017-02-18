46951d1a4f48aae74c8f5bbc2598e545906a3549
rr- <rr-@sakuya.pl>
2017-02-18 11:22:30 +0100

    mod/term/tmux: add plugin manager

--- mod/term/tmux/install.py
+++ mod/term/tmux/install.py
@@ -1,7 +1,13 @@
-import packages
-import util
+from os.path import expanduser
+from dotinstall import packages
+from dotinstall import util
 
 
 def run():
     packages.try_install('tmux')
     util.create_symlink('./tmux.conf', '~/.tmux.conf')
+    util.run_verbose([
+        'git',
+        'clone',
+        'https://github.com/tmux-plugins/tpm',
+        expanduser('~/.config/tmux/plugins/tpm')])
