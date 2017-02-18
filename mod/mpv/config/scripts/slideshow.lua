a92a392567c241f305def5b94ed7298a148d5b33
Dash <dash@bad.pink>
2017-02-18 18:04:04 +0100

    mod/mpv: add ability to control slideshow speed

--- mod/mpv/config/scripts/slideshow.lua
+++ mod/mpv/config/scripts/slideshow.lua
@@ -1,6 +1,11 @@
 local on = false
 local expected_playlist_pos  = 0
 local timer = nil
+local slideshow_sleep = mp.get_opt('slideshow-duration')
+
+if slideshow_sleep == nil then
+    slideshow_sleep = 0.3
+end
 
 function get_playlist_pos()
     return mp.get_property_number('playlist-pos') + 1
@@ -22,7 +27,7 @@ function queue_next_slide()
     if timer then
         timer:kill()
     end
-    timer = mp.add_timeout(0.3, next_slide)
+    timer = mp.add_timeout(slideshow_sleep, next_slide)
 end
 
 function start_slideshow()
@@ -53,6 +58,19 @@ function toggle_slideshow()
     end
 end
 
+function increase_slideshow_speed()
+    slideshow_sleep = slideshow_sleep + 0.1
+    mp.osd_message('Slideshow speed: ' .. slideshow_sleep)
+end
+
+function decrease_slideshow_speed()
+    slideshow_sleep = slideshow_sleep - 0.1
+    if slideshow_sleep < 0.1 then
+        slideshow_sleep = 0.1
+    end
+    mp.osd_message('Slideshow speed: ' .. slideshow_sleep)
+end
+
 function file_loaded()
     if on then
         expected_playlist_pos = expected_playlist_pos + 1
@@ -62,3 +80,5 @@ end
 
 mp.register_event('file-loaded', file_loaded)
 mp.register_script_message('toggle-slideshow', toggle_slideshow)
+mp.register_script_message('decrease-speed', decrease_slideshow_speed)
+mp.register_script_message('increase-speed', increase_slideshow_speed)
