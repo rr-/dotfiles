ec1b9ca2ccc539ad05ebb6ea6526c80f480b1db5
rr- <rr-@sakuya.pl>
2017-03-17 20:09:52 +0100

    mod/mpi: extract from mod/mpv

---
+++ mod/mpi/install.py
@@ -0,0 +1,10 @@
+from dotinstall import packages
+from dotinstall import util
+
+
+def run():
+    packages.try_install('luajit')
+    if not util.has_executable('mpv'):
+        packages.try_install('mpv-git')
+    util.create_symlink('./config', '~/.config/mpi')
+    util.create_symlink('./mpi', '~/.local/bin/')
