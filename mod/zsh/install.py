9c732b96b9f18e7376fc29d12963a867da1723f5
rr- <rr-@sakuya.pl>
2017-03-25 10:52:07 +0100

    mod/zsh: add readline shortcuts to less

--- mod/zsh/install.py
+++ mod/zsh/install.py
@@ -1,3 +1,4 @@
+from os.path import expanduser
 from dotinstall import packages
 from dotinstall import util
 
@@ -6,3 +7,5 @@ def run():
     packages.try_install('zsh')
     util.create_symlink('./zshrc', '~/.zshrc')
     util.create_dir('~/.config/zsh')
+
+    util.run_verbose(['lesskey', '-o', expanduser('~/.less'), '--', './lesskey'])
