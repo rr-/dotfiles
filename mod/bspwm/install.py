3220282e8a9919e6f8d7a14d443da869714b6e9a
rr- <rr-@sakuya.pl>
2017-04-02 20:37:00 +0200

    mod/bpswm: move panel dependencies to mod/panel

--- mod/bspwm/install.py
+++ mod/bspwm/install.py
@@ -3,14 +3,12 @@ from dotinstall import util
 
 
 def run():
-    packages.try_install('libxkbcommon-x11')  # fix qt5
-    packages.try_install('python-pyqt5')      # for panel
-    packages.try_install('dmenu')             # program executor
     if not util.has_executable('bspwm'):
-        packages.try_install('bspwm-git')     # window manager
-    packages.try_install('sxhkd-git')         # hotkey manager
-    packages.try_install('xdo-git')           # like xdotool, but different
-    packages.try_install('feh')               # wallpaper renderer
+        packages.try_install('bspwm-git')  # window manager
+    packages.try_install('dmenu')          # program executor
+    packages.try_install('sxhkd-git')      # hotkey manager
+    packages.try_install('xdo-git')        # like xdotool, but different
+    packages.try_install('feh')            # wallpaper renderer
 
     util.create_symlink('./sxhkdrc', '~/.config/sxhkd/')
     util.create_symlink('./bspwmrc', '~/.config/bspwm/')
