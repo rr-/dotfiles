7ccd1a2ac2530fa3d532594e1de6ed40de155ed1
rr- <rr-@sakuya.pl>
2017-04-02 20:32:43 +0200

    mod/sxhkd: extract from mod/bspwm

---
+++ mod/sxhkd/install.py
@@ -0,0 +1,10 @@
+from dotinstall import packages
+from dotinstall import util
+
+
+def run():
+    packages.try_install('sxhkd-git')
+    packages.try_install('xdo-git')
+
+    util.create_symlink('./config', '~/.config/sxhkd')
+    util.create_symlink('./beep-shot.mp3', '~/.local/share/')
