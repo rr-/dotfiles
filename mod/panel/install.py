4e1664e6802b78ed7ff5dabde5849a5d0639efa7
Dash <dash@bad.pink>
2017-04-02 20:37:00 +0200

    mod/bpswm: move panel dependencies to mod/panel

--- mod/panel/install.py
+++ mod/panel/install.py
@@ -2,5 +2,8 @@ from dotinstall import util
 
 
 def run():
+    packages.try_install('libxkbcommon-x11')
+    packages.try_install('python-pyqt5')
+
     util.create_symlink('./panel', '~/.local/bin/')
     util.run_verbose(['sudo', 'pip', 'install', '-r', 'panel/requirements.txt'])
