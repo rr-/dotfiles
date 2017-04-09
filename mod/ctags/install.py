248c01d6777e70e7a180809ef8ff27580dc7b499
Dash <dash@bad.pink>
2017-04-09 22:31:14 +0200

    remove unused cruft

--- mod/ctags/install.py
+++
@@ -1,7 +0,0 @@
-from dotinstall import packages
-from dotinstall import util
-
-
-def run():
-    packages.try_install('ctags')
-    util.create_symlink('./ctags', '~/.ctags')
--- mod/term/xfce/install.py
+++
@@ -1,7 +0,0 @@
-from dotinstall import packages
-from dotinstall import util
-
-
-def run():
-    packages.try_install('xfce4-terminal')
-    util.create_symlink('./config', '~/.config/xfce4/terminal')
