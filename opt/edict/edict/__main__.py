0b7ce5b1fe08719ed8822a51194c71859b6c8d65
Dash <dash@bad.pink>
2017-05-16 21:31:29 +0200

    opt/edict: add --tags argument

--- opt/edict/edict/__main__.py
+++ opt/edict/edict/__main__.py
@@ -15,6 +15,7 @@ _RAW_PATH = pathlib.Path('~/.local/cache/edict2.txt').expanduser()
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser('Look up words in edict2 dictionary')
     parser.add_argument('pattern', nargs='+', help='regex to search for')
+    parser.add_argument('--tags', nargs='*', help='regex to search tags for')
     return parser.parse_args()
 
 
@@ -40,9 +41,16 @@ def create_db_if_needed() -> None:
 def main() -> None:
     args = parse_args()
     patterns: t.List[str] = args.pattern
+    tag_patterns: t.List[str] = args.tags or []
 
     create_db_if_needed()
     entries = db.search_entries_by_regex(patterns)
+    entries = [
+        entry
+        for entry in entries
+        if all(
+            tag_pattern.lower() in [tag.lower() for tag in entry.tags]
+            for tag_pattern in tag_patterns)]
 
     for entry in entries:
         print('({})'.format(','.join(entry.tags)))
