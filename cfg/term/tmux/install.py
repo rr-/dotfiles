9d8255997dd549efb05946b6ffead914c9c2d9c3
Dash <dash@bad.pink>
2017-05-17 11:32:33 +0200

    cfg/term/tmux: use neutral colors
    Previously tmux used blue colors for remote session and golden colors
    for local session. Now it uses grey colors.

--- cfg/term/tmux/install.py
+++ cfg/term/tmux/install.py
@@ -6,9 +6,6 @@ from dotinstall import util
 def run():
     packages.try_install('tmux')
     util.create_symlink('./tmux.conf', '~/.tmux.conf')
-    util.create_symlink('./colors.conf', '~/.config/tmux/')
-    util.create_symlink('./ssh.conf', '~/.config/tmux/')
-    util.create_symlink('./non-ssh.conf', '~/.config/tmux/')
     util.run_verbose([
         'git',
         'clone',
