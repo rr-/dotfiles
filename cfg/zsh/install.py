82f43156d921cf8a44729aed07144c7dbb695ae2
Dash <dash@bad.pink>
2017-06-01 23:21:57 +0200

    cfg/zsh: extract zshenv

--- cfg/zsh/install.py
+++ cfg/zsh/install.py
@@ -6,6 +6,8 @@ from dotinstall import util
 def run():
     packages.try_install('zsh')
     util.create_symlink('./zshrc', '~/.zshrc')
+    util.create_symlink('./zprofile', '~/.zprofile')
+    util.create_symlink('./zshenv', '~/.zshenv')
     util.create_dir('~/.config/zsh')
 
     util.run_verbose(['lesskey', '-o', expanduser('~/.less'), '--', './lesskey'])
