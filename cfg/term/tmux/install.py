62214f44b4bd6acc0ab72b8e3593ab48802f9822
rr- <rr-@sakuya.pl>
2017-06-14 13:55:48 +0200

    colors: foundation
    - use dark theme by default
    - change program settings to use dark theme
    - use f11/f12 in tmux to switch to light/dark theme by remapping palette

--- cfg/term/tmux/install.py
+++ cfg/term/tmux/install.py
@@ -6,6 +6,7 @@ from dotinstall import util
 def run():
     packages.try_install('tmux')
     util.create_symlink('./tmux.conf', '~/.tmux.conf')
+    util.create_symlink('./config', '~/.config/tmux')
     util.run_verbose([
         'git',
         'clone',
