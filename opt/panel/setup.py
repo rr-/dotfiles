bbf6b1985dcb3b0ed2bb0b24cfc304db54670174
rr- <rr-@sakuya.pl>
2017-08-24 18:24:28 +0200

    cfg/panel: move to opt/panel

---
+++ opt/panel/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    author='rr-',
+    author_email='rr-@sakuya.pl',
+    name='panel',
+    long_description='lemonbar on heavy steroids',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['panel = panel.__main__:main']},
+    package_dir={'panel': 'panel'},
+    package_data={'panel': ['data/**/*']},
+    install_requires=[
+        'PyQT5',
+        'python-xlib',
+        'pyalsaaudio',
+        'psutil',
+    ])
