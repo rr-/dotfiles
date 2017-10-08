214e9066228892ed51e4e5c524e69cd0791f485f
Dash <dash@bad.pink>
2017-10-08 17:58:26 +0200

    cfg/bubblesub/qc: use py3.6 formatting

--- cfg/bubblesub/scripts/qc.py
+++ cfg/bubblesub/scripts/qc.py
@@ -15,21 +15,23 @@ def _check_durations(logger, line):
         return
 
     if line.duration < MIN_DURATION:
-        logger.info('#{}: duration shorter than {} ms'.format(
-            line.number, MIN_DURATION))
+        logger.info(
+            f'#{line.number}: duration shorter than {MIN_DURATION} ms')
 
     if line.duration < MIN_DURATION_LONG \
             and bubblesub.util.character_count(text) >= 8:
-        logger.info('#{}: duration shorter than {} ms'.format(
-            line.number, MIN_DURATION_LONG))
+        logger.info(
+            f'#{line.number}: '
+            f'duration shorter than {MIN_DURATION_LONG} ms')
 
     next_line = line.next
     while next_line:
         if bubblesub.util.ass_to_plaintext(next_line.text):
             gap = next_line.start - line.end
             if gap > 0 and gap < MIN_GAP:
-                logger.info('#{}+#{}: gap shorter than {} ms ({} ms)'.format(
-                    line.number, next_line.number, MIN_GAP, gap))
+                logger.info(
+                    f'#{line.number}+#{next_line.number}: '
+                    f'gap shorter than {MIN_GAP} ms ({gap} ms)')
             return
         next_line = next_line.next
 
@@ -38,25 +40,23 @@ def _check_punctuation(logger, line):
     text = bubblesub.util.ass_to_plaintext(line.text)
 
     if ' \\N' in text:
-        logger.info('#{}: space before line break'.format(line.number))
+        logger.info(f'#{line.number}: space before line break')
 
     if '  ' in text:
-        logger.info('#{}: double space'.format(line.number))
+        logger.info(f'#{line.number}: double space')
 
     if '...' in text:
-        logger.info('#{}: bad ellipsis (expected …)'.format(line.number))
+        logger.info(f'#{line.number}: bad ellipsis (expected …)')
 
     if re.search('… *[,.]', text):
-        logger.info('#{}: comma or dot after ellipsis'.format(line.number))
+        logger.info(f'#{line.number}: extra comma or dot after ellipsis')
 
     if PUNCTUATION_INSIDE_QUOTES == 1:
         if re.search('[^.][.,]"', line.text):
-            logger.info('#{}: period/comma inside quotation mark'.format(
-                line.number))
+            logger.info(f'#{line.number}: period/comma inside quotation mark')
     elif PUNCTUATION_INSIDE_QUOTES == 1:
         if re.search('"[.,][^.]', line.text) or re.search('"[.,]$', line.text):
-            logger.info('#{}: period/comma outside quotation mark'.format(
-                line.number))
+            logger.info(f'#{line.number}: period/comma outside quotation mark')
 
     context = re.split(r'\W+', re.sub('[.,?!"]', '', line.text.lower()))
     for word in (
@@ -65,19 +65,19 @@ def _check_punctuation(logger, line):
         'dont', 'wouldnt', 'couldnt', 'shouldnt', 'hasnt', 'havent', 'ive'
     ):
         if word in context:
-            logger.info('#{}: missing apostrophe'.format(line.number))
+            logger.info(f'#{line.number}: missing apostrophe')
 
     if re.search(r'[-–](\\N|$)', text):
-        logger.info('#{}: bad dash (expected —)'.format(line.number))
+        logger.info(f'#{line.number}: bad dash (expected —)')
 
     if re.search(r'(^|\\N)–[^–]*$', text):
-        logger.info('#{}: dialog with just one person'.format(line.number))
+        logger.info(f'#{line.number}: dialog with just one person')
 
     if re.search(r'(^|\\N)(- |—)', text):
-        logger.info('#{}: bad dash (expected –)'.format(line.number))
+        logger.info(f'#{line.number}: bad dash (expected –)')
 
     if re.search(r'(^|\\N)[A-Z][a-z]{,3}-[a-z]', text):
-        logger.info('#{}: wrong stutter capitalization'.format(line.number))
+        logger.info(f'#{line.number}: possible wrong stutter capitalization')
 
 
 def _check_malformed_tags(logger, line):
@@ -103,15 +103,13 @@ def _check_malformed_tags(logger, line):
         return re.match(r'^\d+(\.\d+)?$', text)
 
     if r'\}' in line.text:
-        logger.info('#{}: malformed tag (slash before brace)'.format(
-            line.number))
+        logger.info(f'#{line.number}: malformed tag (slash before brace)')
 
     if r'{\\' in line.text:
-        logger.info('#{}: malformed tag (double slash)'.format(line.number))
+        logger.info(f'#{line.number}: malformed tag (double slash)')
 
     if re.match('{[^}]+$', line.text):
-        logger.info('#{}: malformed tag (unterminated brace)'.format(
-            line.number))
+        logger.info(f'#{line.number}: malformed tag (unterminated brace)')
 
     for match in re.finditer(r'\\([0-9]*[a-z]+)([^\\}]*)[\\}]', line.text):
         tag = match.group(1)
@@ -123,8 +121,8 @@ def _check_malformed_tags(logger, line):
         ):
             if arg:
                 logger.info(
-                    '#{}: malformed {} tag (expected no arguments)'.format(
-                        line.number, tag))
+                    f'#{line.number}: '
+                    f'malformed {tag} tag (expected no arguments)')
 
         elif tag in (
             'i',  # italics
@@ -132,23 +130,23 @@ def _check_malformed_tags(logger, line):
             's',  # strike-through
         ):
             if _validate_boolean(arg):
-                logger.info('#{}: malformed %s tag (not a boolean)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not a boolean)')
 
         elif tag == 'an':  # new style anchors
             if arg not in '123456789':
-                logger.info('#{}: malformed %s tag (bad alignment)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (bad alignment)')
 
         elif tag == 'a':  # old style anchors
             if arg not in ('1', '2', '3', '5', '6', '7', '9', '10', '11'):
-                logger.info('#{}: malformed %s tag (bad alignment)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (bad alignment)')
 
         elif tag == 'q':  # wrap style
             if arg not in ('1', '2', '3', '4'):
-                logger.info('#{}: malformed %s tag (bad wrap style)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (bad wrap style)')
 
         elif tag in (
             'b'      # bold (weights)
@@ -163,8 +161,8 @@ def _check_malformed_tags(logger, line):
             'kf',    # karaoke (sweep + hide borders)
         ):
             if not _validate_integer(arg, allow_negative=False):
-                logger.info('#{}: malformed %s tag (not an integer)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not an integer)')
 
         elif tag in (
             'shad',   # shadow
@@ -174,8 +172,8 @@ def _check_malformed_tags(logger, line):
             'ybord',  # border y
         ):
             if not _validate_float(arg, allow_negative=False):
-                logger.info('#{}: malformed %s tag (not a number)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not a number)')
 
         elif tag in (
             'xshad',  # shadow x
@@ -188,24 +186,23 @@ def _check_malformed_tags(logger, line):
             'fay',    # font shear y
         ):
             if not _validate_float(arg, allow_negative=True):
-                logger.info('#{}: malformed %s tag (not a number)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not a number)')
 
         elif tag in ('c', '1c', '2c', '3c', '4c'):  # colors
             if not _validate_color(arg):
-                logger.info('#{}: malformed {} tag (not a color)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not a color)')
 
         elif tag in ('alpha', '1a', '2a', '3a', '4a'):  # alpha
             if not _validate_opacity(arg):
-                logger.info('#{}: malformed {} tag (not an opacity)'.format(
-                    line.number, tag))
+                logger.info(
+                    f'#{line.number}: malformed {tag} tag (not an opacity)')
 
         elif tag in ('pos', 'move', 'org', 'fad', 'fade'):
             if not arg.startswith('(') or not arg.endswith(')'):
                 logger.info(
-                    '#{}: malformed {} tag (missing braces)'.format(
-                        line.number, tag))
+                    f'#{line.number}: malformed {tag} tag (missing braces)')
             else:
                 arg = arg[1:-1]
                 parts = arg.split(',')
@@ -213,8 +210,8 @@ def _check_malformed_tags(logger, line):
                         _validate_integer(part, allow_negative=True)
                         for part in parts):
                     logger.info(
-                        '#{}: malformed {} tag (not an integer)'.format(
-                            line.number, tag))
+                        f'#{line.number}: '
+                        f'malformed {tag} tag (not an integer)')
                 if tag == 'fade':
                     expected_part_count = (7,)
                 elif tag == 'move':
@@ -223,8 +220,8 @@ def _check_malformed_tags(logger, line):
                     expected_part_count = (2,)
                 if len(parts) not in expected_part_count:
                     logger.info(
-                        '#{}: malformed {} tag (invalid part count)'.format(
-                            line.number, tag))
+                        f'#{line.number}: '
+                        f'malformed {tag} tag (invalid part count)')
 
         elif tag not in (
             'fn',     # font name
@@ -234,13 +231,12 @@ def _check_malformed_tags(logger, line):
             'iclip',  # mask path (inverse clip)
             # TODO: drawing tags
         ):
-            logger.info(
-                '#{}: unknown tag ({})'.format(line.number, tag))
+            logger.info(f'#{line.number}: unknown tag ({tag})')
 
 
 def _check_disjointed_tags(logger, line):
     if '}{' in line.text:
-        logger.info('#{}: disjointed tags'.format(line.number))
+        logger.info(f'#{line.number}: disjointed tags')
 
 
 def _check_broken_comments(logger, line):
@@ -249,15 +245,15 @@ def _check_broken_comments(logger, line):
             or '}' in striped_text \
             or re.search('}[^{]}', line.text) \
             or re.search('{[^}]{', line.text):
-        logger.info('#{}: broken comment'.format(line.number))
+        logger.info(f'#{line.number}: broken comment')
 
 
 def _check_double_words(logger, line):
     text = bubblesub.util.ass_to_plaintext(line.text)
 
     for pair in re.finditer(r'(?<!\w)(\w+)\s+\1(?!\w)', text):
-        logger.info('#{}: double word ({})'.format(
-            line.number, pair.group(1)))
+        word = pair.group(1)
+        logger.info(f'#{line.number}: double word ({word})')
 
 
 class QualityCheckCommand(bubblesub.api.cmd.PluginCommand):
