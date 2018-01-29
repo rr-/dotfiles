ffb923cc5940470783d3a21325e4d257d9365608
Dash <dash@bad.pink>
2018-01-29 16:55:33 +0100

    cfg/bubblesub: add fade to white

--- cfg/bubblesub/scripts/fade.py
+++ cfg/bubblesub/scripts/fade.py
@@ -1,9 +1,17 @@
 import re
+from enum import Enum
+import bubblesub.util
 from bubblesub.api.cmd import PluginCommand
 
 
 BLACK = (16, 16, 16)
+WHITE = (255, 255, 255)
 DURATION = 2000
+
+
+class Direction(Enum):
+    Left = -1
+    Right = 1
 
 
 def format_color(number, color):
@@ -28,9 +36,25 @@ def format_ass_tags(*tags, close=True):
     return joined
 
 
-class FadeFromBlackCommand(PluginCommand):
-    name = 'grid/fade-from-black'
-    menu_name = '&Fade from black'
+class FadeCommand:
+    @bubblesub.util.classproperty
+    def name(self):
+        ret = 'grid/fade-'
+        ret += str(self.direction) + '-'
+        ret += str(self.color)
+        return ret
+
+    @bubblesub.util.classproperty
+    def duration(self):
+        raise NotImplementedError('Unknown duration')
+
+    @bubblesub.util.classproperty
+    def direction(self):
+        raise NotImplementedError('Unknown direction')
+
+    @bubblesub.util.classproperty
+    def color(self):
+        raise NotImplementedError('Unknown color')
 
     @property
     def is_enabled(self):
@@ -43,38 +67,46 @@ class FadeFromBlackCommand(PluginCommand):
             c2 = style.secondary_color
             c3 = style.outline_color
             c4 = style.back_color
-            line.text = format_ass_tags(
-                format_color(1, BLACK),
-                format_color(3, BLACK),
-                format_animation(
-                    0,
-                    DURATION,
+
+            if self.direction == Direction.Left:
+                line.text = format_ass_tags(
+                    format_color(1, self.color),
+                    format_color(3, self.color),
+                    format_animation(
+                        0,
+                        self.duration,
+                        format_color(1, c1),
+                        format_color(3, c3)),
+                    close=True) + line.text
+            elif self.direction == Direction.Right:
+                line.text = format_ass_tags(
                     format_color(1, c1),
-                    format_color(3, c3)),
-                close=True) + line.text
+                    format_color(3, c3),
+                    format_animation(
+                        max(0, line.duration - self.duration),
+                        line.duration,
+                        format_color(1, self.color),
+                        format_color(3, self.color)),
+                    close=True) + line.text
+            else:
+                raise ValueError('Invalid direction')
 
 
-class FadeToBlackCommand(PluginCommand):
-    name = 'grid/fade-to-black'
-    menu_name = '&Fade to black'
+def define_cmd(menu_name, color, direction):
+    type(
+        'CustomFadeCommand',
+        (FadeCommand, PluginCommand),
+        {
+            'direction': direction,
+            'color': color,
+            'menu_name': menu_name,
+            'duration': DURATION,
+        })
 
-    @property
-    def is_enabled(self):
-        return self.api.subs.has_selection
 
-    async def run(self):
-        for line in self.api.subs.selected_lines:
-            style = self.api.subs.styles.get_by_name(line.style)
-            c1 = style.primary_color
-            c2 = style.secondary_color
-            c3 = style.outline_color
-            c4 = style.back_color
-            line.text = format_ass_tags(
-                format_color(1, c1),
-                format_color(3, c3),
-                format_animation(
-                    max(0, line.duration - DURATION),
-                    line.duration,
-                    format_color(1, BLACK),
-                    format_color(3, BLACK)),
-                close=True) + line.text
+for menu_name, color, direction in [
+        ('Fade from &black', BLACK, Direction.Left),
+        ('Fade from &white', WHITE, Direction.Left),
+        ('Fade to &black', BLACK, Direction.Right),
+        ('Fade to &white', WHITE, Direction.Right)]:
+    define_cmd(menu_name, color, direction)
