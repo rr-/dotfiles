d227b960040f9863d106eca1b2a75843bfec7d05
Dash <dash@bad.pink>
2018-03-22 10:58:53 +0100

    cfg/mpi: adapt vifm's new --remote-expr

--- cfg/mpi/config/scripts/run_vifm.js
+++ cfg/mpi/config/scripts/run_vifm.js
@@ -15,10 +15,8 @@ function get_vifm_server_name() {
     return mp.utils.getenv('VIFM_SERVER_NAME');
 }
 
-// TODO: https://github.com/vifm/vifm/issues/293
 function get_vifm_info(callback) {
     var vifm_server_name = get_vifm_server_name();
-    var tmp_file = '/tmp/tmp.txt';
 
     args = ['vifm'];
     if (vifm_server_name) {
@@ -26,26 +24,18 @@ function get_vifm_info(callback) {
         args.push(vifm_server_name);
     }
     args = args.concat([
-        '--remote',
-        '-c', 'execute "!echo " paneisat("left") ">' + tmp_file + '"',
-        '-c', '!echo %c >>' + tmp_file,
-        '-c', '!echo %C >>' + tmp_file,
-        '-c', '!echo %d >>' + tmp_file,
-        '-c', '!echo %D >>' + tmp_file,
+        '--remote-expr',
+        'paneisat("left").expand("\\n%c\\n%C\\n%d\\n%D")',
     ]);
     var result = mp.utils.subprocess({args: args, cancellable: false});
-
-    setTimeout(function() {
-        var lines = mp.utils.read_file(tmp_file).split(/\n/);
-        var info = {
-            selected_pane: parseInt(lines[0]),
-            current_file: lines[1],
-            other_file: lines[2],
-            current_dir: lines[3],
-            other_dir: lines[4],
-        };
-        callback(info);
-    }, 100);
+    var lines = result.stdout.split(/\n/);
+    return {
+        selected_pane: parseInt(lines[0]),
+        current_file: lines[1],
+        other_file: lines[2],
+        current_dir: lines[3],
+        other_dir: lines[4],
+    };
 }
 
 function run_vifm() {
@@ -55,24 +45,23 @@ function run_vifm() {
     }
 
     var vifm_server_name = get_vifm_server_name();
-    get_vifm_info(function(vifm_info) {
-        var args = ['vifm'];
-        if (vifm_server_name) {
-            args.push('--server-name');
-            args.push(vifm_server_name);
-        }
-        args.push('--remote');
-        if (vifm_info.selected_pane === 1) {
-            args.push('--select');
-            args.push(last_known_path);
-        } else {
-            args.push(vifm_info.other_dir);
-            args.push('--select');
-            args.push(last_known_path);
-        }
-        mp.utils.subprocess_detached({args: args});
-        mp.command('quit');
-    });
+    var vifm_info = get_vifm_info();
+    var args = ['vifm'];
+    if (vifm_server_name) {
+        args.push('--server-name');
+        args.push(vifm_server_name);
+    }
+    args.push('--remote');
+    if (vifm_info.selected_pane === 1) {
+        args.push('--select');
+        args.push(last_known_path);
+    } else {
+        args.push(vifm_info.other_dir);
+        args.push('--select');
+        args.push(last_known_path);
+    }
+    mp.utils.subprocess_detached({args: args});
+    mp.command('quit');
 }
 
 mp.register_script_message('run-vifm', run_vifm);
