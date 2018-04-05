231c00f186d41c2dc7c54d3f72465fe809dbfdea
Dash <dash@bad.pink>
2018-04-05 10:01:10 +0200

    cfg/bspwm: support locking screen

--- cfg/bspwm/install.py
+++ cfg/bspwm/install.py
@@ -7,6 +7,7 @@ def run():
         packages.try_install('bspwm-git')  # window manager
     packages.try_install('dmenu')          # program executor
     packages.try_install('feh')            # wallpaper renderer
+    packages.try_install('i3lock')         # lock screen
 
     util.create_symlink('./bspwmrc', '~/.config/bspwm/')
     util.create_symlink('./rules', '~/.config/bspwm/')
