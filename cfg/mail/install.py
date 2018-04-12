edc342fa154b315a8675ba06ed5b2d5f39445101
rr- <rr-@sakuya.pl>
2018-04-12 23:24:33 +0200

    cfg/mail: switch to maildir

--- cfg/mail/install.py
+++ cfg/mail/install.py
@@ -3,6 +3,11 @@ from dotinstall import util
 
 
 def run():
+    packages.try_install('getmail')
+    util.create_dir('~/mail/new')
+    util.create_dir('~/mail/cur')
+    util.create_dir('~/mail/tmp')
+
     packages.try_install('neomutt')
     packages.try_install('w3m')
     packages.try_install('lynx')
@@ -10,6 +15,7 @@ def run():
     util.create_file('~/.mutt/certificates')
     util.create_dir('~/.mutt/cache/bodies')
     util.create_dir('~/.mutt/cache/headers')
+
     util.create_symlink('./file_email', '~/.mutt/')
     util.create_symlink('./colors.muttrc', '~/.mutt/')
     util.create_symlink('./muttrc', '~/.mutt/')
