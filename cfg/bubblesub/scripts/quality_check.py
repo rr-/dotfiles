52d986b440e1ec878696e22b18f7b0895a8c06a2
rr- <rr-@sakuya.pl>
2018-05-13 17:06:47 +0200

    cfg/bubblesub/qc: check for spaces before . and ,

--- cfg/bubblesub/scripts/quality_check.py
+++ cfg/bubblesub/scripts/quality_check.py
@@ -54,6 +54,9 @@ def _check_punctuation(logger, line):
     if re.search(r'\n\s|\s\n', text):
         logger.warn(f'#{line.number}: space around line break')
 
+    if re.search(r'\s[.,]', text):
+        logger.warn(f'#{line.number}: space before period/comma')
+
     if '  ' in text:
         logger.warn(f'#{line.number}: double space')
 
