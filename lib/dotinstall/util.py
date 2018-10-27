e887cc035077c3099ab79d007a72233cf140ad09
rr- <rr-@sakuya.pl>
2018-10-27 15:43:07 +0200

    lib: use double quotes

--- lib/dotinstall/util.py
+++ lib/dotinstall/util.py
@@ -14,7 +14,7 @@ def run_silent(*args, **kwargs):
     proc = Popen(stdout=PIPE, stderr=PIPE, *args, **kwargs)
     out, err = proc.communicate()
     try:
-        out, err = out.decode('utf8'), err.decode('utf8')
+        out, err = out.decode("utf8"), err.decode("utf8")
     except UnicodeDecodeError:
         pass
     return (proc.returncode == 0, out, err)
@@ -30,11 +30,11 @@ def has_executable(program):
 
 def assert_has_executable(program):
     if not has_executable(program):
-        raise RuntimeError('%s not installed, cannot proceed', program)
+        raise RuntimeError("%s not installed, cannot proceed", program)
 
 
 def expand_path(path, source=None):
-    is_dir = path.endswith('/') or path.endswith('\\')
+    is_dir = path.endswith("/") or path.endswith("\\")
     target = os.path.abspath(os.path.expanduser(path))
     if is_dir and source:
         target = os.path.join(target, os.path.basename(source))
@@ -48,7 +48,7 @@ def abs_path(path):
 def find(path):
     path = abs_path(path)
     if os.path.isdir(path):
-        return glob.glob(os.path.join(path, '*'))
+        return glob.glob(os.path.join(path, "*"))
     return glob.glob(path)
 
 
@@ -58,12 +58,12 @@ def download(url, path, overwrite=False):
         path = os.path.join(path, os.path.basename(url))
     create_dir(os.path.dirname(path))
     if overwrite or not os.path.exists(path):
-        logger.info('Downloading %r into %r...', url, path)
+        logger.info("Downloading %r into %r...", url, path)
         request = urllib.request.Request(url)
-        request.add_header('User-Agent', 'mozilla')
-        request.add_header('Referer', url)
+        request.add_header("User-Agent", "mozilla")
+        request.add_header("Referer", url)
         response = urllib.request.urlopen(request)
-        with open(path, 'wb') as handle:
+        with open(path, "wb") as handle:
             handle.write(response.read())
 
 
@@ -77,8 +77,8 @@ def create_file(path, content=None, overwrite=False):
     dir_path = os.path.dirname(path)
     create_dir(dir_path)
     if overwrite or not os.path.exists(path):
-        logger.info('Creating file %r...', path)
-        with open(path, 'w') as handle:
+        logger.info("Creating file %r...", path)
+        with open(path, "w") as handle:
             if content:
                 handle.write(content)
 
@@ -86,7 +86,7 @@ def create_file(path, content=None, overwrite=False):
 def create_dir(path):
     path = expand_path(path)
     if not os.path.exists(path):
-        logger.info('Creating directory %r...', path)
+        logger.info("Creating directory %r...", path)
         os.makedirs(path)
 
 
@@ -94,7 +94,7 @@ def copy_file(source, target):
     source = expand_path(source)
     target = expand_path(target, source)
     _remove_symlink(target)
-    logger.info('Copying %r to %r...', source, target)
+    logger.info("Copying %r to %r...", source, target)
     create_dir(os.path.dirname(target))
     shutil.copy(source, target)
 
@@ -105,14 +105,14 @@ def create_symlink(source, target):
     _remove_symlink(target)
     if os.path.exists(target):
         raise RuntimeError(
-            'Target file %r exists and is not a symlink.', target
+            "Target file %r exists and is not a symlink.", target
         )
-    logger.info('Linking %r to %r...', source, target)
+    logger.info("Linking %r to %r...", source, target)
     create_dir(os.path.dirname(target))
     os.symlink(source, target)
 
 
 def _remove_symlink(path):
     if os.path.islink(path):
-        logger.info('Removing old symlink %r...', path)
+        logger.info("Removing old symlink %r...", path)
         os.unlink(path)
