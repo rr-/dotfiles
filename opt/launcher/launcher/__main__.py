f2d05a87d50996c59f6c4bd3779ef6172e1ee05e
Dash <dash@bad.pink>
2018-10-27 15:37:35 +0200

    bin/launcher: use double quotes

--- opt/launcher/launcher/__main__.py
+++ opt/launcher/launcher/__main__.py
@@ -9,26 +9,26 @@ from pathlib import Path
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('file')
+    parser.add_argument("file")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    for line in Path('~/.config/launcher.json').expanduser().open():
-        pattern, command = line.split('::')
+    for line in Path("~/.config/launcher.json").expanduser().open():
+        pattern, command = line.split("::")
         pattern = pattern.strip()
         command = command.strip()
         if re.match(pattern, args.file, re.I):
             parameters = shlex.split(command)
-            parameters = [args.file if p == '{}' else p for p in parameters]
-            if '{}' not in parameters:
+            parameters = [args.file if p == "{}" else p for p in parameters]
+            if "{}" not in parameters:
                 parameters.append(args.file)
             subprocess.run(parameters)
             sys.exit(0)
-    print('No suitable program found')
+    print("No suitable program found")
     sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- opt/launcher/setup.py
+++ opt/launcher/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    author='rr-',
-    author_email='rr-@sakuya.pl',
-    name='launcher',
-    long_description='xdg-open replacement',
+    author="rr-",
+    author_email="rr-@sakuya.pl",
+    name="launcher",
+    long_description="xdg-open replacement",
     packages=find_packages(),
-    entry_points={'console_scripts': ['xdg-open = launcher.__main__:main']})
+    entry_points={"console_scripts": ["xdg-open = launcher.__main__:main"]},
+)
