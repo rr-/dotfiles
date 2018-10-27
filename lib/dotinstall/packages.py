d6fb7a2fbd93eefb2b8d154f8a30d51321af5a98
rr- <rr-@sakuya.pl>
2018-10-27 15:43:00 +0200

    lib: reformat code

--- lib/dotinstall/packages.py
+++ lib/dotinstall/packages.py
@@ -1,13 +1,14 @@
-import sys
 import logging
-import tempfile
 import re
+import sys
+import tempfile
+
 from dotinstall import util
 
 logger = logging.getLogger(__name__)
 
 
-class CygwinPackageInstaller(object):
+class CygwinPackageInstaller:
     name = 'cygwin'
 
     @property
@@ -15,18 +16,21 @@ class CygwinPackageInstaller(object):
         return util.has_executable('apt-cyg')
 
     def has_installed(self, package):
-        return len(
-            util.run_silent(['apt-cyg', 'list', '^%s$' % package])[1]) > 0
+        return (
+            len(util.run_silent(['apt-cyg', 'list', '^%s$' % package])[1]) > 0
+        )
 
     def is_available(self, package):
-        return len(
-            util.run_silent(['apt-cyg', 'listall', '^%s$' % package])[1]) > 0
+        return (
+            len(util.run_silent(['apt-cyg', 'listall', '^%s$' % package])[1])
+            > 0
+        )
 
     def install(self, package):
         return util.run_verbose(['apt-cyg', 'install', package])
 
 
-class PacmanPackageInstaller(object):
+class PacmanPackageInstaller:
     name = 'pacman'
 
     @property
@@ -43,7 +47,7 @@ class PacmanPackageInstaller(object):
         return util.run_verbose(['sudo', '-S', 'pacman', '-S', package])
 
 
-class PacaurPackageInstaller(object):
+class PacaurPackageInstaller:
     name = 'pacaur'
 
     @property
@@ -57,11 +61,12 @@ class PacaurPackageInstaller(object):
         return util.run_silent(['pacaur', '-Ss', package])[0]
 
     def install(self, package):
-        return util.run_verbose([
-            'pacaur', '-S', package, '--noconfirm', '--noedit'])
+        return util.run_verbose(
+            ['pacaur', '-S', package, '--noconfirm', '--noedit']
+        )
 
 
-class YaourtPackageInstaller(object):
+class YaourtPackageInstaller:
     name = 'yaourt'
 
     @property
@@ -78,7 +83,7 @@ class YaourtPackageInstaller(object):
         return util.run_verbose(['yaourt', '-S', package])
 
 
-class PipPackageInstaller(object):
+class PipPackageInstaller:
     name = 'pip'
     cache_dir = tempfile.gettempdir()
 
@@ -97,22 +102,41 @@ class PipPackageInstaller(object):
         return util.has_executable(self.executable)
 
     def has_installed(self, package):
-        return re.search(
-            '^' + re.escape(package) + r'($|\s)',
-            util.run_silent([self.executable, 'list'])[1],
-            re.MULTILINE) is not None
+        return (
+            re.search(
+                '^' + re.escape(package) + r'($|\s)',
+                util.run_silent([self.executable, 'list'])[1],
+                re.MULTILINE,
+            )
+            is not None
+        )
 
     def is_available(self, package):
-        return re.search(
-            '^' + re.escape(package) + r'($|\s)',
-            util.run_silent([
-                self.executable, 'search', package,
-                '--cache-dir', self.cache_dir])[1],
-            re.MULTILINE) is not None
+        return (
+            re.search(
+                '^' + re.escape(package) + r'($|\s)',
+                util.run_silent(
+                    [
+                        self.executable,
+                        'search',
+                        package,
+                        '--cache-dir',
+                        self.cache_dir,
+                    ]
+                )[1],
+                re.MULTILINE,
+            )
+            is not None
+        )
 
     def install(self, package):
         command = [
-            self.executable, 'install', '--cache-dir', self.cache_dir, package]
+            self.executable,
+            'install',
+            '--cache-dir',
+            self.cache_dir,
+            package,
+        ]
         if self.use_sudo:
             command = ['sudo', '-S'] + command
         return util.run_verbose(command)
@@ -154,14 +178,17 @@ def install(package, method=None):
             logger.info(
                 'Package %s is available, installing with %s',
                 package,
-                installer.name)
+                installer.name,
+            )
             return installer.install(package)
     if method is None:
         raise RuntimeError(
-            'No package manager is capable of installing %s', package)
+            'No package manager is capable of installing %s', package
+        )
     else:
         raise RuntimeError(
-            '%s is not capable of installing %s', method, package)
+            '%s is not capable of installing %s', method, package
+        )
 
 
 def _choose_installers(method):
@@ -173,7 +200,8 @@ def _choose_installers(method):
     if len(chosen_installers) == 0:
         if method is None:
             raise RuntimeError(
-                'No package manager is supported on this system!')
+                'No package manager is supported on this system!'
+            )
         else:
             raise RuntimeError('%s is not supported on this system!', method)
     return chosen_installers
