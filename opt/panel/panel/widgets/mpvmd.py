4537f4ed0e0929979ba209df80768d80b63b8db4
rr- <rr-@sakuya.pl>
2018-10-27 02:13:47 +0200

    opt/panel: update to new mpvmc

--- opt/panel/panel/widgets/mpvmd.py
+++ opt/panel/panel/widgets/mpvmd.py
@@ -1,11 +1,12 @@
 import os
 import math
 import json
-import struct
 import socket
-from typing import Optional, Dict
+from typing import List, Any
 from PyQt5 import QtWidgets
 from panel.widgets.widget import Widget
+
+SOCKET_PATH = '/tmp/mpvmd.socket'
 
 
 def _format_time(seconds):
@@ -13,13 +14,129 @@ def _format_time(seconds):
     return '%02d:%02d' % (seconds // 60, seconds % 60)
 
 
+class Info:
+    def __init__(self):
+        self.raw = {}
+
+    @property
+    def path(self):
+        return self.raw.get('path', None)
+
+    @property
+    def pause(self):
+        return self.raw.get('pause', False)
+
+    @property
+    def metadata(self):
+        return {
+            key.lower(): value
+            for key, value in (self.raw.get('metadata') or {}).items()
+        }
+
+    @property
+    def elapsed(self):
+        return self.raw.get('time-pos', 0)
+
+    @property
+    def duration(self):
+        return self.raw.get('duration', 0)
+
+    @property
+    def random_playback(self):
+        return (
+            self.raw
+            .get('script-opts', {})
+            .get('random_playback', 'no')
+        ) == 'yes'
+
+
+class Connection:
+    def __init__(self):
+        self._request_id = 0
+        self._socket = None
+        self.info = Info()
+
+    @property
+    def connected(self):
+        return self._socket is not None
+
+    def connect(self):
+        if self.connected:
+            return
+
+        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._socket.settimeout(3)
+        self._socket.setblocking(True)
+
+        try:
+            self._socket.connect(SOCKET_PATH)
+        except ConnectionRefusedError:
+            self._socket = None
+            raise
+
+        self._request_id = 1
+
+        for i, name in enumerate(
+            [
+                'pause',
+                'time-pos',
+                'duration',
+                'script-opts',
+                'metadata',
+                'path',
+            ],
+            1,
+        ):
+            self.send(['observe_property', i, name])
+
+    def send(self, command: List[str]) -> None:
+        message = {'command': command, 'request_id': self._request_id}
+        self._send(message)
+        self._request_id += 1
+
+    def process(self):
+        if not self.connected:
+            return
+
+        for event in self._recv():
+            if event.get('event') != 'property-change':
+                continue
+            self.info.raw[event['name']] = event['data']
+
+    def _send(self, message: Any) -> None:
+        print('out', message)
+        try:
+            self._socket.send((json.dumps(message) + '\n').encode())
+        except (BrokenPipeError, ValueError):
+            self._socket = None
+            raise
+
+    def _recv(self) -> List[Any]:
+        data = b''
+        try:
+            while True:
+                chunk = self._socket.recv(1024)
+                data += chunk
+                if chunk.endswith(b'\n') or not chunk:
+                    break
+        except (BrokenPipeError, ValueError):
+            self._socket = None
+            raise
+        ret = []
+        for line in data.decode().split('\n'):
+            if line:
+                ret.append(json.loads(line))
+        print('in', ret)
+        return ret
+
+
 class MpvmdWidget(Widget):
-    delay = 0.5
+    delay = 0
 
     def __init__(self, app, main_window):
         super().__init__(app, main_window)
-        self._socket = None
-        self._info = None
+        self._connection = Connection()
+        self._info = self._connection.info
 
         self._container = QtWidgets.QWidget()
         self._status_icon_label = QtWidgets.QLabel(self._container)
@@ -37,110 +154,92 @@ class MpvmdWidget(Widget):
         self._status_icon_label.wheelEvent = self._prev_or_next_track
         self._song_label.wheelEvent = self._prev_or_next_track
 
-        try:
-            self._refresh_impl()
-        except Exception:
-            return
-
     @property
     def container(self):
         return self._container
 
     @property
     def available(self):
-        return self._info is not None
+        return os.path.exists(SOCKET_PATH)
 
     def _play_pause_clicked(self, _event):
         with self.exception_guard():
-            if self._info['paused']:
-                self._send({'msg': 'play'})
+            if self._info.pause:
+                self._connection.send(['set_property', 'pause', 'no'])
             else:
-                self._send({'msg': 'pause'})
-            self._recv()
+                self._connection.send(['set_property', 'pause', 'yes'])
             self.refresh()
             self.render()
 
     def _prev_or_next_track(self, event):
         with self.exception_guard():
-            if event.angleDelta().y() > 0:
-                self._send({'msg': 'playlist-next'})
-            else:
-                self._send({'msg': 'playlist-prev'})
-            self._recv()
+            self._connection.send(
+                [
+                    'script-message-to',
+                    'playlist',
+                    'playlist-next'
+                    if event.angleDelta().y() > 0
+                    else 'playlist-prev',
+                ]
+            )
             self.refresh()
             self.render()
 
     def _shuffle_clicked(self, _event):
         with self.exception_guard():
-            self._send({'msg': 'random', 'random': not self._info['random']})
-            self._recv()
+            data = self._info.raw['script-opts'].copy()
+            data['random_playback'] = (
+                'no' if self._info.random_playback else 'yes'
+            )
+            self._connection.send(['set_property', 'script-opts', data])
+
             self.refresh()
             self.render()
 
     def _refresh_impl(self):
-        if not self._socket:
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                self._socket.settimeout(3)
-                self._socket.connect(('localhost', 36934))
-            except ConnectionRefusedError:
-                self.delay = min(60, self.delay + 1)
-                raise
-
         try:
-            self._send({'msg': 'info'})
-            self._info = self._recv()
-            self.delay = 0.5
-        except (BrokenPipeError, ValueError):
-            self._socket = None
+            if not self._connection.connected:
+                self._connection.connect()
+            self._connection.process()
+        except (ConnectionRefusedError, BrokenPipeError, ValueError):
             self.delay = min(60, self.delay + 1)
             raise
+        else:
+            self.delay = 0
 
     def _render_impl(self):
-        if self._info['paused']:
+        if self._info.pause:
             self._set_icon(self._status_icon_label, 'pause')
         else:
             self._set_icon(self._status_icon_label, 'play')
 
-        metadata = {k.lower(): v for k, v in self._info['metadata'].items()}
-
-        elapsed = self._info['time-pos']
-        duration = self._info['duration']
-
         text = ''
-        if metadata.get('title'):
-            if metadata.get('artist'):
-                text = metadata['artist'] + ' - ' + metadata['title']
+        if self._info.metadata.get('title'):
+            if self._info.metadata.get('artist'):
+                text = (
+                    self._info.metadata['artist']
+                    + ' - '
+                    + self._info.metadata['title']
+                )
             else:
-                text = metadata['title']
-        elif metadata.get('icy-title'):
-            text = metadata['icy-title']
+                text = self._info.metadata['title']
+        elif self._info.metadata.get('icy-title'):
+            text = self._info.metadata['icy-title']
         else:
-            text = os.path.basename(self._info['path'] or '')
+            text = os.path.basename(self._info.path or '')
 
-        if elapsed and duration:
+        if self._info.elapsed and self._info.duration:
             text += ' %s / %s' % (
-                _format_time(elapsed), _format_time(duration))
+                _format_time(self._info.elapsed),
+                _format_time(self._info.duration),
+            )
 
         self._song_label.setText(text)
 
-        shuffle = self._info['random']
+        shuffle = self._info.random_playback
         if self._shuffle_icon_label.property('active') != shuffle:
             self._shuffle_icon_label.setProperty('active', shuffle)
             if shuffle:
                 self._set_icon(self._shuffle_icon_label, 'shuffle-on')
             else:
                 self._set_icon(self._shuffle_icon_label, 'shuffle-off')
-
-    def _recv(self) -> Optional[Dict]:
-        data_size_raw = self._socket.recv(4)
-        if not data_size_raw:
-            return None
-        data_size = struct.unpack('<I', data_size_raw)[0]
-        data = self._socket.recv(data_size)
-        return json.loads(data.decode('utf-8'))
-
-    def _send(self, message: Dict):
-        data = json.dumps(message).encode('utf-8')
-        self._socket.send(struct.pack('<I', len(data)))
-        self._socket.send(data)
