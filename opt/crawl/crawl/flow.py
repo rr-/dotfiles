1b46a6e92efa62e25349788d1fa138065f468e1c
Dash <dash@bad.pink>
2018-10-29 23:49:33 +0100

    opt/crawl: improve support for ^c

---
+++ opt/crawl/crawl/flow.py
@@ -0,0 +1,22 @@
+import concurrent.futures
+import contextlib
+import typing as T
+
+
+class Flow:
+    shutdown = False
+
+    @staticmethod
+    @contextlib.contextmanager
+    def guard(executor: concurrent.futures.Executor) -> T.Generator:
+        try:
+            yield
+        except KeyboardInterrupt:
+            Flow.shutdown = True
+            executor.shutdown(wait=False)
+            raise InterruptedError
+
+    @staticmethod
+    def check():
+        if Flow.shutdown:
+            raise InterruptedError
