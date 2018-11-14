2abfc3ae7c0dd0a0c24a2bcc78684d5ed206c150
rr- <rr-@sakuya.pl>
2018-11-14 12:36:16 +0100

    cfg/search: don't use git version
    This is to avoid having to install rust and cargo.

--- cfg/search/install.py
+++ cfg/search/install.py
@@ -5,5 +5,5 @@ from dotinstall import util
 def run():
     packages.try_install('fzf')  # super opener
     packages.try_install('silver-searcher-git')  # super grep (vim-fzf dependency)
-    packages.try_install('ripgrep-git')  # super grep (shell)
+    packages.try_install('ripgrep')  # super grep (shell)
     util.create_symlink('./agignore', '~/.agignore')
