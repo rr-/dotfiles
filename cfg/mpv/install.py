d5e8eb210bec4d1bc9ab60afc433fce7cd19159f
rr- <rr-@sakuya.pl>
2018-11-18 14:20:42 +0100

    cfg/mpv: use binary mpv version

--- cfg/mpv/install.py
+++ cfg/mpv/install.py
@@ -5,6 +5,5 @@ from dotinstall import util
 def run():
     packages.try_install('mujs')
     packages.try_install('luajit')
-    if not util.has_executable('mpv'):
-        packages.try_install('mpv-git')
+    packages.try_install('mpv')
     util.create_symlink('./config', '~/.config/mpv')
