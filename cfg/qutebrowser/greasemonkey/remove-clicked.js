77f2b067cbb4be6b52412f6510fc35f5ef94c9da
Dash <dash@bad.pink>
2018-12-27 14:50:01 +0100

    cfg/qutebrowser/remove-clicked: don't depend on jquery

--- cfg/qutebrowser/greasemonkey/remove-clicked.js
+++ cfg/qutebrowser/greasemonkey/remove-clicked.js
@@ -4,16 +4,15 @@
 // @version      1.0
 // @author       rr-
 // @include      *
-// @require      http://code.jquery.com/jquery-1.8.0.min.js
 // @run-at       document-start|document-end
 // ==/UserScript==
 
 (function() {
-    $(document).click(function (e) {
-        if (e.ctrlKey) {
-            var element = e.target;
-            $(element).remove();
-            e.stopPropagation();
+    document.addEventListener('click', function (event) {
+        if (event.ctrlKey) {
+            var element = event.target;
+            element.parentNode.removeChild(element);
+            event.stopPropagation();
             return false;
         }
     });
