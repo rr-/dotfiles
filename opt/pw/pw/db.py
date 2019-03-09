8f3480b6b70629ee0bcbdaaed2dff2237122f904
Dash <dash@bad.pink>
2019-03-09 18:55:34 +0100

    opt/pw: add cmd to change master password

--- opt/pw/pw/db.py
+++ opt/pw/pw/db.py
@@ -21,27 +21,31 @@ class PasswordDoesNotExist(RuntimeError):
         super().__init__("password does not exist")
 
 
+def read_database(key: str) -> T.Dict[str, T.Dict[str, str]]:
+    aes = AESCipher(key)
+    if not DB_FILE.exists():
+        return {}
+    return json.loads(
+        zlib.decompress(aes.decrypt(DB_FILE.read_bytes())).decode()
+    )
+
+
+def write_database(key: str, db: T.Dict[str, T.Dict[str, str]]) -> None:
+    aes = AESCipher(key)
+    DB_FILE.write_bytes(
+        aes.encrypt(
+            zlib.compress(
+                json.dumps(db).encode(), level=zlib.Z_BEST_COMPRESSION
+            )
+        )
+    )
+
+
 @contextlib.contextmanager
 def database() -> T.Iterator[T.Dict[str, T.Dict[str, str]]]:
     key = getpass("Master password: ")
-    aes = AESCipher(key)
-
-    db: T.Dict[str, T.Dict[str, str]]
-    if DB_FILE.exists():
-        db = json.loads(
-            zlib.decompress(aes.decrypt(DB_FILE.read_bytes())).decode()
-        )
-    else:
-        db = {}
-
+    db = read_database(key)
     old_db = copy.deepcopy(db)
-
     yield db
     if old_db != db:
-        DB_FILE.write_bytes(
-            aes.encrypt(
-                zlib.compress(
-                    json.dumps(db).encode(), level=zlib.Z_BEST_COMPRESSION
-                )
-            )
-        )
+        write_database(key, db)
