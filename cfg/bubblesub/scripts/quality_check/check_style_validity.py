e1430ab0ab976867f219786bc238051e52a14799
rr- <rr-@sakuya.pl>
2019-06-06 22:29:06 +0200

    cfg/bubblesub/qc: allow special styles in comments

--- cfg/bubblesub/scripts/quality_check/check_style_validity.py
+++ cfg/bubblesub/scripts/quality_check/check_style_validity.py
@@ -9,6 +9,13 @@ from .common import BaseResult, Violation
 def check_style_validity(
     event: AssEvent, styles: AssStyleList
 ) -> T.Iterable[BaseResult]:
+    if (
+        event.style.startswith("[")
+        and event.style.endswith("]")
+        and event.is_comment
+    ):
+        return
+
     style = styles.get_by_name(event.style)
     if style is None:
         yield Violation(event, f"using non-existing style")
