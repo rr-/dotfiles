e8aa6cd15366ef329628bc3b3e5b7faf2a75c448
rr- <rr-@sakuya.pl>
2019-06-18 14:01:56 +0200

    cfg/pacaur: update dependencies

--- cfg/pacaur/install.py
+++ cfg/pacaur/install.py
@@ -5,14 +5,17 @@ from dotinstall import util
 
 def run():
     packages.install('base-devel', method='pacman')
-    packages.install('yajl', method='pacman')
+    packages.install('expac', method='pacman')
+    packages.install('meson', method='pacman')
+    packages.install('gtest', method='pacman')
+    packages.install('gmock', method='pacman')
+    packages.install('jq', method='pacman')
+    packages.install('sudo', method='pacman')
 
     os.chdir('/tmp')
-    util.run_verbose(['git', 'clone', 'https://aur.archlinux.org/cower.git'])
-    os.chdir('/tmp/cower')
+    util.run_verbose(['git', 'clone', 'https://aur.archlinux.org/auracle-git.git'])
+    os.chdir('/tmp/auracle-git')
     util.run_verbose(['makepkg', '-i', '--skippgpcheck'])
-
-    packages.install('expac', method='pacman')
 
     os.chdir('/tmp')
     util.run_verbose(['git', 'clone', 'https://aur.archlinux.org/pacaur.git'])
