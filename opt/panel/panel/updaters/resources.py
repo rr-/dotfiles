bc5f95dd0243cbc7f9d9f56972fd2676dfdb386a
Dash <dash@bad.pink>
2019-07-03 17:04:49 +0200

    opt/panel: avoid creating a thread

--- opt/panel/panel/updaters/resources.py
+++ opt/panel/panel/updaters/resources.py
@@ -4,33 +4,12 @@ from PyQt5 import QtCore
 from panel.updaters.base import BaseUpdater
 
 
-class CpuUpdaterThread(QtCore.QThread):
-    value_changed = QtCore.pyqtSignal(float)
-
-    def __init__(self, parent: QtCore.QObject) -> None:
-        super().__init__(parent)
-        self.running = False
-
-    def run(self) -> None:
-        value = psutil.cpu_percent(interval=0.1)
-        self.value_changed.emit(value)
-
-        self.running = True
-        while self.running:
-            value = psutil.cpu_percent(interval=1)
-            self.value_changed.emit(value)
-
-
 class ResourcesUpdater(BaseUpdater):
+    cpu_usage_changed = QtCore.pyqtSignal(float)
     ram_usage_changed = QtCore.pyqtSignal(float)
 
     def __init__(self) -> None:
         super().__init__()
-
-        self.cpu_thread = CpuUpdaterThread(self)
-        self.cpu_thread.start()
-
-        self.cpu_usage_changed = self.cpu_thread.value_changed
 
         timer = QtCore.QTimer(self)
         timer.setInterval(1000)
@@ -38,4 +17,5 @@ class ResourcesUpdater(BaseUpdater):
         timer.start()
 
     def _on_timeout(self) -> None:
+        self.cpu_usage_changed.emit(psutil.cpu_percent(interval=None))
         self.ram_usage_changed.emit(psutil.virtual_memory().percent)
