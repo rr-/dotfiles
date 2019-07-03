60522d56438bf33ce10e466c96977400fd79ea31
rr- <rr-@sakuya.pl>
2019-07-03 16:45:50 +0200

    opt/panel: refactor widgets (network)

--- opt/panel/panel/widgets/net.py
+++ opt/panel/panel/widgets/net.py
@@ -1,13 +1,11 @@
-import glob
 import math
-import typing as T
-from pathlib import Path
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtWidgets
 
 from panel.colors import Colors
+from panel.updaters.network import NetworkUpdater
+from panel.widgets.base import BaseWidget
 from panel.widgets.chart import Chart
-from panel.widgets.widget import Widget
 
 
 def convert_speed(speed_bytes: int) -> str:
@@ -19,68 +17,26 @@ def convert_speed(speed_bytes: int) -> str:
     return f"{speed_bytes / denominator:.0f} {suffixes[power]}"
 
 
-class NetworkUsageWidget(Widget):
-    delay = 1
-
+class NetworkUsageWidget(BaseWidget):
     def __init__(
-        self, app: QtWidgets.QApplication, main_window: QtWidgets.QWidget
+        self, updater: NetworkUpdater, parent: QtWidgets.QWidget
     ) -> None:
-        super().__init__(app, main_window)
-        self.net_in = 0
-        self.net_out = 0
-        self._old_rx_bytes = 0
-        self._old_tx_bytes = 0
+        super().__init__(parent)
+        self._updater = updater
 
-        available = False
-        try:
-            self._rx_path: T.Optional[Path] = None
-            self._tx_path: T.Optional[Path] = None
-            for path in Path("/sys/class/net/").iterdir():
-                if "virtual" in str(path.resolve()):
-                    continue
-                state = (path / "operstate").read_text().strip()
-                if state.lower() == "up":
-                    self._rx_path = path / "statistics" / "rx_bytes"
-                    self._tx_path = path / "statistics" / "tx_bytes"
-                    available = True
-        except Exception:
-            pass
-
-        if available:
-            self._old_rx_bytes = int(self._rx_path.read_text().strip())
-            self._old_tx_bytes = int(self._tx_path.read_text().strip())
-
-        self._container = QtWidgets.QWidget(main_window)
         self._chart = Chart(
-            self._container,
-            min_width=120,
-            scale_low=0.0,
-            scale_high=1024.0 * 1024.0,
+            self, min_width=120, scale_low=0.0, scale_high=1024.0 * 1024.0
         )
 
-        layout = QtWidgets.QHBoxLayout(self._container, margin=0, spacing=6)
+        layout = QtWidgets.QHBoxLayout(self, margin=0, spacing=6)
         layout.addWidget(self._chart)
 
-    @property
-    def container(self) -> QtWidgets.QWidget:
-        return self._container
+        self._updater.updated.connect(self._on_update)
 
-    @property
-    def available(self) -> bool:
-        return self._rx_path is not None and self._tx_path is not None
-
-    def _refresh_impl(self) -> None:
-        rx_bytes = int(self._rx_path.read_text().strip())
-        tx_bytes = int(self._tx_path.read_text().strip())
-        self.net_in = rx_bytes - self._old_rx_bytes
-        self.net_out = tx_bytes - self._old_tx_bytes
-        self._old_rx_bytes = rx_bytes
-        self._old_tx_bytes = tx_bytes
-
-    def _render_impl(self) -> None:
+    def _on_update(self, net_in: int, net_out: int) -> None:
         self._chart.setLabel(
-            f"DL {convert_speed(self.net_in)}  UL {convert_speed(self.net_out)}"
+            f"DL {convert_speed(net_in)}  UL {convert_speed(net_out)}"
         )
-        self._chart.addPoint(Colors.net_up_chart_line, self.net_in)
-        self._chart.addPoint(Colors.net_down_chart_line, self.net_out)
+        self._chart.addPoint(Colors.net_up_chart_line, net_in)
+        self._chart.addPoint(Colors.net_down_chart_line, net_out)
         self._chart.update()
