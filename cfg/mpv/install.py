0ce2fadd46411e8dfb066dd1fb1f0a6a7dfcbc2c
rr- <rr-@sakuya.pl>
2019-08-21 00:32:50 +0200

    cfg/mpv: support linux mint

--- cfg/mpv/install.py
+++ cfg/mpv/install.py
@@ -2,7 +2,8 @@ from dotinstall import packages, util
 
 
 def run():
-    packages.try_install("mujs")
-    packages.try_install("luajit")
+    if util.distro_name() == "arch":
+        packages.try_install("mujs")
+        packages.try_install("luajit")
     packages.try_install("mpv")
     util.create_symlink("./config", "~/.config/mpv")
