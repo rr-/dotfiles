1b75c0aa8e41dbfb5f41834671067d52018d1047
Dash <dash@bad.pink>
2019-08-21 00:10:54 +0200

    lib/dotinstall: refactor

--- lib/dotinstall/packages.py
+++ lib/dotinstall/packages.py
@@ -1,101 +1,121 @@
 import logging
 import re
 import sys
+import typing as T
 
 from dotinstall import util
 
 logger = logging.getLogger(__name__)
 
 
-class CygwinPackageInstaller:
+class PackageInstaller:
+    @property
+    def name(self) -> str:
+        raise NotImplementedError("not implemented")
+
+    @property
+    def is_supported(self) -> bool:
+        raise NotImplementedError("not implemented")
+
+    def has_installed(self, package: str) -> bool:
+        raise NotImplementedError("not implemented")
+
+    def is_available(self, package: str) -> bool:
+        raise NotImplementedError("not implemented")
+
+    def install(self, package: str) -> bool:
+        raise NotImplementedError("not implemented")
+
+
+class CygwinPackageInstaller(PackageInstaller):
     name = "cygwin"
 
     @property
-    def supported(self):
+    def is_supported(self) -> bool:
         return util.has_executable("apt-cyg")
 
-    def has_installed(self, package):
+    def has_installed(self, package: str) -> bool:
         return (
             len(util.run_silent(["apt-cyg", "list", "^%s$" % package])[1]) > 0
         )
 
-    def is_available(self, package):
+    def is_available(self, package: str) -> bool:
         return (
             len(util.run_silent(["apt-cyg", "listall", "^%s$" % package])[1])
             > 0
         )
 
-    def install(self, package):
+    def install(self, package: str) -> bool:
         return util.run_verbose(["apt-cyg", "install", package])
 
 
-class AptPackageInstaller:
+class AptPackageInstaller(PackageInstaller):
     name = "apt"
 
     @property
-    def supported(self):
+    def is_supported(self) -> bool:
         return util.has_executable("apt")
 
-    def has_installed(self, package):
+    def has_installed(self, package: str) -> bool:
         return util.run_silent(["dpkg", "-l", package])[0]
 
-    def is_available(self, package):
+    def is_available(self, package: str) -> bool:
         return util.run_silent(["apt", "show", package])[0]
 
-    def install(self, package):
+    def install(self, package: str) -> bool:
         return util.run_verbose(["sudo", "-S", "apt", "install", package])
 
 
-class PacmanPackageInstaller:
+class PacmanPackageInstaller(PackageInstaller):
     name = "pacman"
 
     @property
-    def supported(self):
+    def is_supported(self) -> bool:
         return util.has_executable("pacman") and util.has_executable("sudo")
 
-    def has_installed(self, package):
+    def has_installed(self, package: str) -> bool:
         return util.run_silent(["pacman", "-Q", package])[0]
 
-    def is_available(self, package):
+    def is_available(self, package: str) -> bool:
         return util.run_silent(["pacman", "-Ss", package])[0]
 
-    def install(self, package):
+    def install(self, package: str) -> bool:
         return util.run_verbose(["sudo", "-S", "pacman", "-S", package])
 
 
-class PacaurPackageInstaller:
+class PacaurPackageInstaller(PackageInstaller):
     name = "pacaur"
 
     @property
-    def supported(self):
+    def is_supported(self) -> bool:
         return util.has_executable("pacaur")
 
-    def has_installed(self, package):
+    def has_installed(self, package: str) -> bool:
         return util.run_silent(["pacaur", "-Q", package])[0]
 
-    def is_available(self, package):
+    def is_available(self, package: str) -> bool:
         return util.run_silent(["pacaur", "-Ss", package])[0]
 
-    def install(self, package):
+    def install(self, package: str) -> bool:
         return util.run_verbose(
             ["pacaur", "-S", package, "--noconfirm", "--noedit"]
         )
 
 
-class PipPackageInstaller:
+class PipPackageInstaller(PackageInstaller):
     name = "pip"
 
-    def __init__(self):
+    def __init__(self) -> None:
         if "cygwin" in sys.platform:
             self.executable = "pip3"
         else:
             self.executable = "pip"
 
     @property
-    def supported(self):
+    def is_supported(self) -> bool:
         return util.has_executable(self.executable)
 
-    def has_installed(self, package):
+    def has_installed(self, package: str) -> bool:
         return (
             re.search(
                 "^" + re.escape(package) + r"($|\s)",
@@ -105,59 +125,40 @@ class PipPackageInstaller:
             is not None
         )
 
-    def is_available(self, package):
+    def is_available(self, package: str) -> bool:
         return (
             re.search(
                 "^" + re.escape(package) + r"($|\s)",
-                util.run_silent(
-                    [
-                        self.executable,
-                        "search",
-                        package,
-                    ]
-                )[1],
+                util.run_silent([self.executable, "search", package])[1],
                 re.MULTILINE,
             )
             is not None
         )
 
-    def install(self, package):
-        command = [
-            self.executable,
-            "install",
-            "--user",
-            package,
-        ]
+    def install(self, package: str) -> bool:
+        command = [self.executable, "install", "--user", package]
         return util.run_verbose(command)
 
 
-INSTALLERS = [
-    CygwinPackageInstaller(),
-    PacmanPackageInstaller(),
-    PacaurPackageInstaller(),
-    PipPackageInstaller(),
-    AptPackageInstaller(),
-]
+INSTALLERS = [cls() for cls in PackageInstaller.__subclasses__()]
 
 
-def try_install(package, method=None):
+def try_install(package: str, method: T.Optional[str] = None) -> bool:
     try:
-        install(package, method)
-        return True
+        return install(package, method)
     except Exception as ex:
         logger.info("Error installing %s: %s", package, ex)
         return False
 
 
-def has_installed(package, method=None):
+def has_installed(package: str, method: T.Optional[str] = None) -> bool:
     chosen_installers = _choose_installers(method)
-    for installer in chosen_installers:
-        if installer.has_installed(package):
-            return True
-    return False
+    return any(
+        installer.has_installed(package) for installer in chosen_installers
+    )
 
 
-def install(package, method=None):
+def install(package: str, method: T.Optional[str] = None) -> bool:
     if has_installed(package, method):
         logger.info("Package %s is already installed.", package)
         return True
@@ -172,25 +173,21 @@ def install(package, method=None):
             return installer.install(package)
     if method is None:
         raise RuntimeError(
-            "No package manager is capable of installing %s", package
+            f"No package manager is capable of installing {package}"
         )
-    else:
-        raise RuntimeError(
-            "%s is not capable of installing %s", method, package
-        )
+    raise RuntimeError(f"{method} is not capable of installing {package}")
 
 
-def _choose_installers(method):
+def _choose_installers(method: T.Optional[str]) -> T.List[PackageInstaller]:
     if method is None:
         chosen_installers = INSTALLERS
     else:
         chosen_installers = [i for i in INSTALLERS if i.name == method]
-    chosen_installers = [i for i in chosen_installers if i.supported]
-    if len(chosen_installers) == 0:
+    chosen_installers = [i for i in chosen_installers if i.is_supported]
+    if not chosen_installers:
         if method is None:
             raise RuntimeError(
                 "No package manager is supported on this system!"
             )
-        else:
-            raise RuntimeError("%s is not supported on this system!", method)
+        raise RuntimeError(f"{method} is not supported on this system!")
     return chosen_installers
--- lib/dotinstall/util.py
+++ lib/dotinstall/util.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pathlib
 import shutil
+import typing as T
 import urllib.request
 from subprocess import PIPE, Popen, call
 
@@ -10,7 +11,7 @@ logger = logging.getLogger(__name__)
 root_dir = pathlib.Path(__file__).parent.parent.parent
 
 
-def run_silent(*args, **kwargs):
+def run_silent(*args: T.Any, **kwargs: T.Any) -> T.Tuple[bool, str, str]:
     proc = Popen(stdout=PIPE, stderr=PIPE, *args, **kwargs)
     out, err = proc.communicate()
     try:
@@ -20,20 +21,20 @@ def run_silent(*args, **kwargs):
     return (proc.returncode == 0, out, err)
 
 
-def run_verbose(*args, **kwargs):
+def run_verbose(*args: T.Any, **kwargs: T.Any) -> bool:
     return call(*args, **kwargs) == 0
 
 
-def has_executable(program):
+def has_executable(program: str) -> bool:
     return shutil.which(program) is not None
 
 
-def assert_has_executable(program):
+def assert_has_executable(program: str) -> None:
     if not has_executable(program):
-        raise RuntimeError("%s not installed, cannot proceed", program)
+        raise RuntimeError(f"{program} not installed, cannot proceed")
 
 
-def expand_path(path, source=None):
+def expand_path(path: str, source: T.Optional[str] = None) -> str:
     is_dir = path.endswith("/") or path.endswith("\\")
     target = os.path.abspath(os.path.expanduser(path))
     if is_dir and source:
@@ -41,18 +42,18 @@ def expand_path(path, source=None):
     return target
 
 
-def abs_path(path):
+def abs_path(path: str) -> str:
     return os.path.abspath(expand_path(path))
 
 
-def find(path):
+def find(path: str) -> T.Iterable[str]:
     path = abs_path(path)
     if os.path.isdir(path):
         return glob.glob(os.path.join(path, "*"))
     return glob.glob(path)
 
 
-def download(url, path, overwrite=False):
+def download(url: str, path: str, overwrite: bool = False) -> None:
     path = expand_path(path)
     if os.path.isdir(path):
         path = os.path.join(path, os.path.basename(url))
@@ -67,12 +68,14 @@ def download(url, path, overwrite=False):
             handle.write(response.read())
 
 
-def exists(path):
+def exists(path: str) -> bool:
     path = expand_path(path)
     return os.path.exists(path)
 
 
-def create_file(path, content=None, overwrite=False):
+def create_file(
+    path: str, content: T.Optional[str] = None, overwrite: bool = False
+) -> None:
     path = expand_path(path)
     dir_path = os.path.dirname(path)
     create_dir(dir_path)
@@ -83,14 +86,14 @@ def create_file(path, content=None, overwrite=False):
                 handle.write(content)
 
 
-def create_dir(path):
+def create_dir(path: str) -> None:
     path = expand_path(path)
     if not os.path.exists(path):
         logger.info("Creating directory %r...", path)
         os.makedirs(path)
 
 
-def copy_file(source, target):
+def copy_file(source: str, target: str) -> None:
     source = expand_path(source)
     target = expand_path(target, source)
     _remove_symlink(target)
@@ -99,20 +102,20 @@ def copy_file(source, target):
     shutil.copy(source, target)
 
 
-def create_symlink(source, target):
+def create_symlink(source: str, target: str) -> None:
     source = expand_path(source)
     target = expand_path(target, source)
     _remove_symlink(target)
     if os.path.exists(target):
         raise RuntimeError(
-            "Target file %r exists and is not a symlink.", target
+            f"Target file {target} exists and is not a symlink."
         )
     logger.info("Linking %r to %r...", source, target)
     create_dir(os.path.dirname(target))
     os.symlink(source, target)
 
 
-def _remove_symlink(path):
+def _remove_symlink(path: str) -> None:
     if os.path.islink(path):
         logger.info("Removing old symlink %r...", path)
         os.unlink(path)
