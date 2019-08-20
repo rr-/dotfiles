efecd018ff6476122d0bb8ec8d4b2affb5962449
Dash <dash@bad.pink>
2019-08-21 01:15:08 +0200

    cfg/tl: support linux mint

--- cfg/tl/install.py
+++ cfg/tl/install.py
@@ -1,5 +1,17 @@
-from dotinstall import packages
+import os
+import tempfile
+
+from dotinstall import packages, util
 
 
 def run():
-    packages.try_install("translate-shell-git")
+    if util.distro_name() == "arch":
+        packages.try_install("translate-shell-git")
+    elif util.distro_name() == "linuxmint":
+        os.chdir(tempfile.gettempdir())
+        util.run_verbose(
+            ["git", "clone", "https://github.com/soimort/translate-shell"]
+        )
+        os.chdir("translate-shell")
+        util.run_verbose(["make"])
+        util.run_verbose(["sudo", "make", "install"])
