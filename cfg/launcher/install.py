2c67d2724d8c8abae3902308ba20e4f5e38b19a1
Dash <dash@bad.pink>
2019-08-21 01:12:21 +0200

    cfg/launcher: support linux mint

--- cfg/launcher/install.py
+++ cfg/launcher/install.py
@@ -1,9 +1,11 @@
 import os
 
-from dotinstall import util
+from dotinstall import packages, util
 
 
 def run():
     util.create_symlink("./launcher.json", "~/.config/launcher.json")
     os.chdir(util.root_dir / "opt" / "launcher")
-    util.run_verbose(["pip", "install", "--user", "--upgrade", "."])
+    util.run_verbose(
+        [packages.PIP_EXECUTABLE, "install", "--user", "--upgrade", "."]
+    )
