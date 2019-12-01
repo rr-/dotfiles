52d69ebf136a279e8f8321251573c751c9f0e25f
Dash <dash@bad.pink>
2019-12-01 15:22:32 +0100

    cfg/bubblesub/sr: add -a option

--- cfg/bubblesub/scripts/google_speech_recognition.py
+++ cfg/bubblesub/scripts/google_speech_recognition.py
@@ -38,43 +38,64 @@ class SpeechRecognitionCommand(BaseCommand):
             await self.args.target.get_subtitles(),
         )
 
-    def run_in_background(self, subs: T.List[AssEvent]) -> None:
+    def run_in_background(self, subtitles: T.List[AssEvent]) -> None:
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            future_to_sub = {
-                executor.submit(self.recognize, sub): sub for sub in subs
-            }
+            if self.args.audio:
+                future = executor.submit(self.recognize_audio_selection)
+                future_to_sub = {future: subtitle for subtitle in subtitles}
+            else:
+                future_to_sub = {
+                    executor.submit(
+                        self.recognize_subtitle, subtitle
+                    ): subtitle
+                    for subtitle in subtitles
+                }
+
             completed, non_completed = concurrent.futures.wait(
                 future_to_sub, timeout=8
             )
 
-        with self.api.undo.capture():
-            for future, sub in future_to_sub.items():
-                if future not in completed:
-                    continue
-                try:
-                    note = future.result()
-                except sr.UnknownValueError:
-                    self.api.log.warn(f"line #{sub.number}: not recognized")
-                except sr.RequestError as ex:
-                    self.api.log.error(f"line #{sub.number}: error ({ex})")
-                else:
-                    self.api.log.info(f"line #{sub.number}: OK")
-                    if sub.note:
-                        sub.note += r"\N" + note
+            with self.api.undo.capture():
+                for future, subtitle in future_to_sub.items():
+                    if future not in completed:
+                        continue
+                    try:
+                        note = future.result()
+                    except sr.UnknownValueError:
+                        self.api.log.warn(
+                            f"line #{subtitle.number}: not recognized"
+                        )
+                    except sr.RequestError as ex:
+                        self.api.log.error(
+                            f"line #{subtitle.number}: error ({ex})"
+                        )
                     else:
-                        sub.note = note
+                        self.api.log.info(f"line #{subtitle.number}: OK")
+                        if subtitle.note:
+                            subtitle.note += r"\N" + note
+                        else:
+                            subtitle.note = note
 
-        for future, sub in future_to_sub.items():
-            if future in non_completed:
-                self.api.log.info(f"line #{sub.number}: timeout")
+            for future, subtitle in future_to_sub.items():
+                if future in non_completed:
+                    self.api.log.info(f"line #{subtitle.number}: timeout")
 
-    def recognize(self, sub: AssEvent) -> str:
-        self.api.log.info(f"line #{sub.number} - analyzing")
+    def recognize_subtitle(self, subtitle: AssEvent) -> str:
+        self.api.log.info(f"line #{subtitle.number} - analyzing")
+        return self.recognize_audio(
+            subtitle.start - LEAD_IN, subtitle.end + LEAD_OUT
+        )
+
+    def recognize_audio_selection(self) -> str:
+        return self.recognize_audio(
+            self.api.audio.view.selection_start - LEAD_IN,
+            self.api.audio.view.selection_end + LEAD_OUT,
+        )
+
+    def recognize_audio(self, start: int, end: int) -> str:
         recognizer = sr.Recognizer()
         with io.BytesIO() as handle:
-            self.api.audio.current_stream.save_wav(
-                handle, sub.start - LEAD_IN, sub.end + LEAD_OUT
-            )
+            self.api.audio.current_stream.save_wav(handle, start, end)
             handle.seek(0, io.SEEK_SET)
             with sr.AudioFile(handle) as source:
                 audio = recognizer.record(source)
@@ -88,6 +109,12 @@ class SpeechRecognitionCommand(BaseCommand):
             help="subtitles to process",
             type=lambda value: SubtitlesSelection(api, value),
             default="selected",
+        )
+        parser.add_argument(
+            "-a",
+            "--audio",
+            help="process only audio selection",
+            action="store_true",
         )
         parser.add_argument("code", help="language code")
 
