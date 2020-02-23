cdbdb54dc9982a1399cadd907bfe015d5479697b
rr- <rr-@sakuya.pl>
2020-02-23 15:57:56 +0100

    opt/pw: add generate command

--- opt/pw/pw/cmd/__init__.py
+++ opt/pw/pw/cmd/__init__.py
@@ -2,6 +2,7 @@ from .base import Command
 from .change_master_password import ChangeMasterPasswordCommand
 from .copy import CopyPasswordCommand
 from .edit import EditDatabaseCommand
+from .get_random_password import GetRandomPasswordCommand
 from .info import InfoPasswordCommand
 from .list import ListPasswordsCommand
 from .set import SetPasswordCommand
