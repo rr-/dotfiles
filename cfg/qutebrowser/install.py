fb385b36f47a6e803609761cc2932087eae7ab96
rr- <rr-@sakuya.pl>
2021-06-19 16:59:25 +0200

    cfg/qutebrowser: delete

--- cfg/qutebrowser/install.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from libdotfiles import packages, util
-
-
-def run():
-    packages.try_install("qutebrowser")
-    util.create_symlink("./config.py", "~/.config/qutebrowser/")
-    util.create_symlink("./greasemonkey", "~/.local/share/qutebrowser/")
