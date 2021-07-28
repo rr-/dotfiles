8df7d54a5e1e5d628ec360f4c42676eb71e6b638
Dash <dash@bad.pink>
2021-07-28 14:04:20 +0200

    cfg/git: use separate identity for work

--- cfg/git/install.py
+++ cfg/git/install.py
@@ -4,5 +4,7 @@ from libdotfiles import packages, util
 def run():
     packages.try_install("git")
     packages.try_install("git-extras-git")
-    util.create_symlink("./gitconfig", "~/.gitconfig")
-    util.create_symlink("./gitignore", "~/.gitignore")
+    util.create_symlink("./gitconfig", "~/.config/git/config")
+    util.create_symlink("./gitconfig-moto", "~/.config/git/config-moto")
+    util.create_symlink("./gitconfig-priv", "~/.config/git/config-priv")
+    util.create_symlink("./gitignore", "~/.config/git/ignore")
