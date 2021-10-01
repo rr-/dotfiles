b5b0a4fb936b3b6bfd7ed07b7215c7ab3867a811
rr- <dash@wind.garden>
2021-10-01 13:37:05 +0200

    cfg/git: move files to a subdirectory

--- cfg/git/__init__.py
+++ cfg/git/__init__.py
@@ -4,7 +4,5 @@ from libdotfiles import packages, util
 def run() -> None:
     packages.try_install("git")
     packages.try_install("git-extras-git")
-    util.create_symlink("./gitconfig", "~/.config/git/config")
-    util.create_symlink("./gitconfig-moto", "~/.config/git/config-moto")
-    util.create_symlink("./gitconfig-priv", "~/.config/git/config-priv")
-    util.create_symlink("./gitignore", "~/.config/git/ignore")
+    util.create_symlink("./config", "~/.config/git")
+    util.create_symlink("./config/ignore", "~/.gitignore")
