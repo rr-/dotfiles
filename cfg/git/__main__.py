27fe0e266b1ff300057636bf968e4129a5de9577
rr- <dash@wind.garden>
2021-10-07 20:59:40 +0200

    cfg/git: add netguru gpg key

--- cfg/git/__main__.py
+++ cfg/git/__main__.py
@@ -4,6 +4,8 @@ from libdotfiles.util import HOME_DIR, PKG_DIR, create_symlinks
 try_install("git")
 try_install("git-extras")
 
+# to generate a new key: gpg --full-generate-key
+
 create_symlinks(
     [
         (PKG_DIR / "config", HOME_DIR / ".config" / "git"),
