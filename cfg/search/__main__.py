ec6a538ab9991ea347c12f123980d47be7372fa6
Dash <dash@bad.pink>
2021-10-07 12:50:29 +0200

    cfg/search: fix installation on Arch

--- cfg/search/__main__.py
+++ cfg/search/__main__.py
@@ -6,9 +6,7 @@ FZF_DIR = HOME_DIR / ".fzf"
 
 if util.distro_name() == "arch":
     packages.try_install("fzf")  # super opener
-    packages.try_install(
-        "silver-searcher-git"
-    )  # super grep (vim-fzf dependency)
+    packages.try_install("the_silver_searcher")  # fzf dependency
     packages.try_install("ripgrep")  # super grep (shell)
 
 elif util.distro_name() == "linuxmint":
