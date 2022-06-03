3b6eed98fc53c063cebbbc0aff00d0901e6b713f
rr- <dash@wind.garden>
2022-06-03 12:12:38 +0200

    cfg/tmux: make git clone idempotent

--- cfg/tmux/__main__.py
+++ cfg/tmux/__main__.py
@@ -1,15 +1,11 @@
 from libdotfiles.packages import try_install
-from libdotfiles.util import HOME_DIR, PKG_DIR, create_symlink, run
+from libdotfiles.util import HOME_DIR, PKG_DIR, create_symlink, git_clone
 
 try_install("tmux")
 create_symlink(PKG_DIR / "tmux.conf", HOME_DIR / ".tmux.conf")
 create_symlink(PKG_DIR / "config", HOME_DIR / ".config" / "tmux")
-run(
-    [
-        "git",
-        "clone",
-        "https://github.com/tmux-plugins/tpm",
-        HOME_DIR / ".config" / "tmux" / "plugins" / "tpm",
-    ],
-    check=True,
+
+git_clone(
+    "https://github.com/tmux-plugins/tpm",
+    HOME_DIR / ".config" / "tmux" / "plugins" / "tpm",
 )
