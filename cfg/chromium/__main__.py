95b8c665c5c34717c3ca295fdeb3905c463c4c2b
rr- <dash@wind.garden>
2022-06-03 14:51:47 +0200

    cfg/chromium: add

---
+++ cfg/chromium/__main__.py
@@ -0,0 +1,12 @@
+from libdotfiles.packages import try_install
+from libdotfiles.util import HOME_DIR, create_file, distro_name
+
+if distro_name() == "arch":
+    try_install("chromium")
+
+create_file(
+    HOME_DIR / ".local" / "bin" / "browser",
+    "#!/bin/sh\nchromium",
+    overwrite=True,
+)
+(HOME_DIR / ".local" / "bin" / "browser").chmod(0o777)
