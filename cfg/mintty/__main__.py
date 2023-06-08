2d1f2718c16f21a311ee70567f8304392a243290
Dash <dash@bad.pink>
2023-06-08 23:56:50 +0200

    cfg/mintty: byebye

--- cfg/mintty/__main__.py
+++
@@ -1,8 +0,0 @@
-from libdotfiles.util import HOME_DIR, PKG_DIR, create_symlinks
-
-create_symlinks(
-    [
-        (PKG_DIR / "inputrc", HOME_DIR / ".inputrc"),
-        (PKG_DIR / "minttyrc", HOME_DIR / ".minttyrc"),
-    ]
-)
