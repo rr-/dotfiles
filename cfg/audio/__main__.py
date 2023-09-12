0c45a1a8f1a485acb36658b5cd2bd525b9b34175
Dash <dash@bad.pink>
2023-09-12 15:44:13 +0200

    audio: improve cycle-audio-device

--- cfg/audio/__main__.py
+++ cfg/audio/__main__.py
@@ -1,9 +1,16 @@
 from libdotfiles.packages import try_install
-from libdotfiles.util import run
+from libdotfiles.util import HOME_DIR, PKG_DIR, copy_file, create_symlink, run
 
 try_install("alsa-utils")
 try_install("pulseaudio")
 try_install("pulseaudio-bluetooth")
 try_install("pavucontrol")
+
+copy_file(
+    PKG_DIR / "audio-sources.yml", HOME_DIR / ".config/audio-sources.yml"
+)
+create_symlink(
+    PKG_DIR / "cycle-audio-device", HOME_DIR / ".local/bin/cycle-audio-device"
+)
 
 run(["pulseaudio", "-D"])
