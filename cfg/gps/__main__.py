49a43d80b0744e3a3b7b60a604488a08ede76a24
Dash <dash@bad.pink>
2023-09-18 13:12:04 +0200

    gps: add

---
+++ cfg/gps/__main__.py
@@ -0,0 +1,21 @@
+from libdotfiles.packages import try_install
+from libdotfiles.util import HOME_DIR, PKG_DIR, create_symlink
+
+try_install("gpsd")
+
+dest1 = HOME_DIR / ".config/google-chrome/NativeMessagingHosts"
+dest2 = HOME_DIR / ".config/chromium/NativeMessagingHosts"
+config_path = PKG_DIR / "au.id.micolous.gpspipe.json"
+config = config_path.read_text().replace(
+    "{PATH}", str(PKG_DIR / "gpspipew.py")
+)
+
+dest1.mkdir(parents=True, exist_ok=True)
+dest2.mkdir(parents=True, exist_ok=True)
+(dest1 / config_path.name).write_text(config)
+(dest2 / config_path.name).write_text(config)
+
+print("To finish, install this extension:")
+print(
+    "https://chrome.google.com/webstore/detail/gpsd-chrome-polyfill/dmfdcjlppdohhegplckcbohgbbfcdfjd"
+)
