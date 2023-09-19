c776efd377343be8de1d614f04776e1ecd212c28
Dash <dash@bad.pink>
2023-09-19 10:23:26 +0200

    audio: fix hp sound drivers on snowflake

--- cfg/audio/__main__.py
+++ cfg/audio/__main__.py
@@ -1,5 +1,12 @@
 from libdotfiles.packages import try_install
-from libdotfiles.util import HOME_DIR, PKG_DIR, copy_file, create_symlink, run
+from libdotfiles.util import (
+    HOME_DIR,
+    PKG_DIR,
+    copy_file,
+    create_symlink,
+    get_hostname,
+    run,
+)
 
 try_install("alsa-utils")
 try_install("pulseaudio")
@@ -12,5 +19,10 @@ copy_file(
 create_symlink(
     PKG_DIR / "cycle-audio-device", HOME_DIR / ".local/bin/cycle-audio-device"
 )
+
+if get_hostname() == "snowflake":
+    try_install("sof-firmware")
+    run(["sudo", "rmmod", "snd_sof_pci_intel_tgl"])
+    run(["sudo", "modprobe", "snd_sof_pci_intel_tgl"])
 
 run(["pulseaudio", "-D"])
