615b223c834bb1615866b51386dea798d1c96294
Dash <dash@bad.pink>
2023-09-21 09:22:23 +0200

    tl: install dict too

--- cfg/tl/__main__.py
+++ cfg/tl/__main__.py
@@ -2,17 +2,40 @@ import os
 import tempfile
 
 from libdotfiles.packages import try_install
-from libdotfiles.util import get_distro_name, run
+from libdotfiles.util import HOME_DIR, get_distro_name, git_clone, run
 
-if get_distro_name() == "arch":
-    try_install("translate-shell-git")
-elif get_distro_name() == "linuxmint":
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        os.chdir(tmp_dir)
-        run(
-            ["git", "clone", "https://github.com/soimort/translate-shell"],
-            check=True,
-        )
-        os.chdir("translate-shell")
-        run(["make"], check=True)
-        run(["sudo", "make", "install"], check=True)
+
+def install_dict() -> None:
+    git_clone("git@github.com:bubblesub/dict", HOME_DIR / "work/priv/dict")
+    run(
+        [
+            "python3",
+            "-m",
+            "pip",
+            "install",
+            "--user",
+            "--break-system-packages",
+            ".",
+        ],
+        check=False,
+        cwd=HOME_DIR / "work/priv/dict",
+    )
+
+
+def install_translate_shell() -> None:
+    if get_distro_name() == "arch":
+        try_install("translate-shell-git")
+    elif get_distro_name() == "linuxmint":
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            os.chdir(tmp_dir)
+            run(
+                ["git", "clone", "https://github.com/soimort/translate-shell"],
+                check=True,
+            )
+            os.chdir("translate-shell")
+            run(["make"], check=True)
+            run(["sudo", "make", "install"], check=True)
+
+
+install_dict()
+install_translate_shell()
