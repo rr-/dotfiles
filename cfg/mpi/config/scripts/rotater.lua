5b118eeb35d516207ca1e473e95307194055a999
Dash <dash@bad.pink>
2024-01-14 17:31:49 +0100

    mpi: update pan scan

--- cfg/mpi/config/scripts/rotater.lua
+++
@@ -1,9 +0,0 @@
-function rotate(number)
-    local rotation = mp.get_property_number('video-rotate')
-    rotation = rotation + number
-    rotation = rotation + 360
-    rotation = rotation % 360
-    mp.set_property('video-rotate', rotation)
-end
-
-mp.register_script_message('rotate', rotate)
