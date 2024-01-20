7a5e493027646637979602f23a6491d5b869d769
Dash <dash@bad.pink>
2024-01-20 23:46:20 +0100

    opt/pysznepl: update

--- opt/pysznepl.js
+++ opt/pysznepl.js
@@ -2,7 +2,7 @@
 // @name         Pyszne.pl augment
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-14
-// @description  Make thumbnails link to hi-res images. Disable endless scroll
+// @description  Link to hi-res thumbnails
 // @author       rr-
 // @match        https://www.pyszne.pl/menu/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pyszne.pl
@@ -10,42 +10,16 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
-
-  function removeEndlessScroll() {
-    if (document.querySelector('head style[data-fix-endless-scroll]')) {
-      return;
-    }
-
-    const cssString = `
-  [data-qa="item-category-list"]>div>div:last-child {
-  position: fixed;
-  left: 50vw;
-  top: 50vh;
-  width: 100px;
-  height: 100px;
-  background: transparent;
-  pointer-events: none;
-}`;
-
-    const styleElement = document.createElement('style');
-    styleElement.innerHTML = cssString;
-    styleElement.dataset.fixEndlessScroll = true;
-    document.head.appendChild(styleElement);
-  }
+  "use strict";
 
   function makePictureLinks($root) {
-    if (!$root.querySelectorAll) {
-      return;
-    }
-
     for (const $imageNode of $root.querySelectorAll(
-      '[data-qa="media"] [data-qa="picture"]>img'
+      ':is([data-qa="item-category-list"], [data-qa="popular-items-list"]) [data-qa="picture"]>img',
     )) {
-      const $linkNode = document.createElement('a');
-      $linkNode.href = $imageNode.src.replace(/([hw])_\d+/g, '$1_1200');
-      $linkNode.target = '_blank';
-      $linkNode.addEventListener('click', (e) => handleImageClick(e));
+      const $linkNode = document.createElement("a");
+      $linkNode.href = $imageNode.src.replace(/([hw])_\d+/g, "$1_1200");
+      $linkNode.target = "_blank";
+      $linkNode.addEventListener("click", (e) => handleImageClick(e));
       const $clonedImageNode = $imageNode.cloneNode();
       $linkNode.appendChild($clonedImageNode);
       $imageNode.parentNode.replaceChild($linkNode, $imageNode);
@@ -55,10 +29,11 @@
   function handleDOMMutation(mutations) {
     for (const mutation of mutations) {
       for (const $node of mutation.addedNodes) {
-        makePictureLinks($node);
+        if ($node.querySelectorAll) {
+          makePictureLinks($node);
+        }
       }
     }
-    removeEndlessScroll();
   }
 
   function handleImageClick(e) {
