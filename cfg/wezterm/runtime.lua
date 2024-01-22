923a16286520538f38938aec20ad08311e6ea800
Dash <dash@bad.pink>
2024-01-22 01:02:53 +0100

    cfg/wezterm: fix hidpi switch support

--- cfg/wezterm/runtime.lua
+++ cfg/wezterm/runtime.lua
@@ -2,6 +2,6 @@ local wezterm = require 'wezterm'
 local module = {}
 function module.apply_to_config(config)
   config.color_scheme = 'dash_light'
-  config.dpi = 96
+  config.font_size = 11
 end
 return module
