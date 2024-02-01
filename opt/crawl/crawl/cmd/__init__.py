d2f09e0db04db4e920883237b308340608d447fb
Dash <dash@bad.pink>
2024-02-02 00:28:06 +0100

    opt/crawl: remove

--- opt/crawl/crawl/cmd/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import BaseCommand
-from .download import DownloadCommand
-from .prune import PruneCommand
-
-__all__ = [
-    "BaseCommand",
-    "DownloadCommand",
-    "PruneCommand",
-]
