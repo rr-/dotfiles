4e35941e9edec87c4a8b7f1f53cfe7d09646a758
Dash <dash@bad.pink>
2024-04-04 13:45:34 +0200

    gps: allow in brave

--- cfg/gps/__main__.py
+++ cfg/gps/__main__.py
@@ -3,17 +3,19 @@ from libdotfiles.util import HOME_DIR, PKG_DIR
 
 try_install("gpsd")
 
-dest1 = HOME_DIR / ".config/google-chrome/NativeMessagingHosts"
-dest2 = HOME_DIR / ".config/chromium/NativeMessagingHosts"
+dests = [
+    HOME_DIR / ".config/google-chrome/NativeMessagingHosts",
+    HOME_DIR / ".config/chromium/NativeMessagingHosts",
+    HOME_DIR / ".config/BraveSoftware/Brave-Browser/NativeMessagingHosts",
+]
 config_path = PKG_DIR / "au.id.micolous.gpspipe.json"
 config = config_path.read_text().replace(
     "{PATH}", str(PKG_DIR / "gpspipew.py")
 )
 
-dest1.mkdir(parents=True, exist_ok=True)
-dest2.mkdir(parents=True, exist_ok=True)
-(dest1 / config_path.name).write_text(config)
-(dest2 / config_path.name).write_text(config)
+for dest in dests:
+    dest.mkdir(parents=True, exist_ok=True)
+    (dest / config_path.name).write_text(config)
 
 print("To finish, install this extension:")
 print(
