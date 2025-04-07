5dc6f68311dc7b758fa30fe8e2622ae4b51c3d2e
Dash <dash@bad.pink>
2025-04-07 11:13:20 +0200

    wezterm: add background texture

--- cfg/wezterm/__main__.py
+++ cfg/wezterm/__main__.py
@@ -5,5 +5,7 @@ cfg_dir = HOME_DIR / ".config" / "wezterm"
 
 try_install("wezterm")
 create_symlink(PKG_DIR / "wezterm.lua", cfg_dir / "wezterm.lua")
+create_symlink(PKG_DIR / "stardust-dark.lua", cfg_dir / "stardust-dark.lua")
+create_symlink(PKG_DIR / "stardust-light.lua", cfg_dir / "stardust-light.lua")
 copy_file(PKG_DIR / "runtime.lua", cfg_dir / "runtime.lua")
 create_symlink(PKG_DIR / "colors", cfg_dir / "colors")
--- cfg/wezterm/runtime.lua
+++ cfg/wezterm/runtime.lua
@@ -2,6 +2,9 @@ local wezterm = require 'wezterm'
 local module = {}
 function module.apply_to_config(config)
   config.color_scheme = 'dash_light'
+  if config.background.source ~= nil then
+    config.background.source.File = '/home/dash/.config/wezterm/stardust-light.png'
+  end
   config.font_size = 11
 end
 return module
