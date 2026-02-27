bd9df7ed1bf3419ca40bb29795d9a179b50c9206
Dash <dash@bad.pink>
2026-02-27 12:55:17 +0100

    nvim: update trx recordings syntax

--- cfg/nvim/nvim/lua/syntax.lua
+++ cfg/nvim/nvim/lua/syntax.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "*.txt",
   callback = function()
-    if vim.fn.search("seed_control", "nw") ~= 0 then
+    if vim.fn.search("seed_control\\|config gameplay", "nw") ~= 0 then
       vim.bo.filetype = "trx_replays"
     end
   end,
